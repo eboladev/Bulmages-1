eb3a11604458f2011911b3ffae50b538e03fb95c
Tomeu Borras <tborras@gmail.com>
2004-01-26 15:58:38 +0000

    *** empty log message ***

---
+++ bulmages/installbulmages/dbmodels/rev-0_3_7.sql
@@ -0,0 +1,61 @@
+-- 
+-- Revision de las bases de datos de empresa. Para BulmaGes 0.3.7
+--
+--            (C)2004, Tomeu Borras tborras@conetxia.com
+--
+-- Este archivo debe aplicarse mediante el comando:
+--
+--     psql xxxx < rev-0_3_7.sql
+--
+-- a todas las empresas del programa BulmaGes.
+--
+-- De esta forma se consigue  actualizar las bases de datos
+-- de la version 0.3.5 a la version 0.3.7
+--
+--
+\connect - postgres
+
+
+-- DROP TABLE linamortizacion;
+-- DROP TABLE amortizacion;
+-- DROP SEQUENCE amortizacion_idamortizacion_seq;
+-- DROP SEQUENCE linamortizacion_idlinamortizacion_seq;
+
+CREATE TABLE amortizacion (
+    idamortizacion 		serial,
+    idcuentaactivo		int4,
+    idcuentaamortizacion	int4,
+    descamortizacion    	character varying(2000),
+    nomamortizacion		character varying(200),
+    fechacompra			date,
+    fecha1cuota			date,
+    valorcompra			float8,
+    periodicidad		int4,
+    numcuotas			int4,
+    metodo			int4,
+    nifproveedor		character varying(12),
+    nomproveedor		character varying(150),
+    dirproveedor		character varying(200),
+    telproveedor		character varying(20),
+    agrupacion			character varying(150),
+    PRIMARY KEY ("idamortizacion"),
+    CONSTRAINT "amortizacion_cuenta_fk" FOREIGN KEY ("idcuentaactivo")
+    REFERENCES "cuenta" ("idcuenta"),
+    CONSTRAINT "amortizacion_cuentaamort_fk" FOREIGN KEY ("idcuentaamortizacion")
+    REFERENCES "cuenta" ("idcuenta")
+);
+
+CREATE TABLE linamortizacion (
+    idlinamortizacion 		serial,
+    idamortizacion		int4,
+    idborrador			int4,
+    ejercicio			int4,
+    fechaprevista		date,
+    cantidad			float8,
+    PRIMARY KEY ("idlinamortizacion"),
+    CONSTRAINT "linamoritzacion_amortizacion_fk" FOREIGN KEY ("idamortizacion")
+    REFERENCES "amortizacion" ("idamortizacion"),
+    CONSTRAINT "linamortizacion_borrador_fk" FOREIGN KEY ("idborrador")
+    REFERENCES "borrador" ("idborrador")
+);    
+    
