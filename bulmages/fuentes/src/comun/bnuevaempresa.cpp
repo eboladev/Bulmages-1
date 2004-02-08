413e743b2bea88cd0c7d329726caa553595ee322
Josep Burcion <josep@burcion.com>
2004-02-08 12:47:44 +0000

    *** empty log message ***

---
+++ bulmages/fuentes/src/comun/bnuevaempresa.cpp
@@ -0,0 +1,68 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Josep Burcion                                   *
+ *   josep@burcion.com                                                     *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#include "bnuevaempresa.h"
+
+
+BNuevaEmpresa::BNuevaEmpresa(QWidget * parent, const char * name, WFlags f)
+ : nuevaempresadlg(parent,name,f)
+{
+}
+
+
+BNuevaEmpresa::~BNuevaEmpresa()
+{
+}
+
+void BNuevaEmpresa::accept()
+{
+  QString nombredb;
+  QString nombreEmp;
+  int ejercicio;
+
+  nombredb = bdempresa->text().stripWhiteSpace()+ejercicioempresa->text().stripWhiteSpace();
+  nombreEmp = nombreempresa->text().stripWhiteSpace();
+  ejercicio = ejercicioempresa->text().stripWhiteSpace().toInt();
+  //Creamos una base de datos a imagen y semejanza de bgplangcont.
+  postgresiface2 *conexionbase;
+  conexionbase = new postgresiface2();
+  conexionbase->inicializa(confpr->valor(CONF_METABASE).c_str());
+  //conexionbase->ejecuta("DROP DATABASE " + nombredb);
+  conexionbase->ejecuta("CREATE DATABASE " + nombredb + " WITH TEMPLATE=bgplangcont");
+   // Aqui vamos a insertar la fila correspondiente en la metabd
+   QString query;
+   cursor2 *cursoraux;
+   conexionbase->begin();
+   query.sprintf("INSERT INTO  empresa (nombre, ano, nombredb) VALUES('%s',%d,'%s')",nombreEmp.latin1(),ejercicio,nombredb.latin1());
+   conexionbase->ejecuta(query);
+   // Agregamos el SUPERUSUARIO "BULMAGES" en la nueva empresa.
+   //Los demas usuarios tendran que ser entrados manualmente por el SUPERUSUARIO.
+   query.sprintf("SELECT idempresa from empresa where nombredb='%s'",nombredb.latin1());
+   cursoraux = conexionbase->cargacursor(query,"cursoraux");
+   int idempresa = cursoraux->valor(0).toInt();
+   query.sprintf("SELECT idusuario from usuario where login='bulmages'");
+   cursoraux = conexionbase->cargacursor(query,"cursoraux");
+   int idSuperUser= cursoraux->valor(0).toInt();
+   query.sprintf("INSERT INTO usuario_empresa(idusuario,idempresa,permisos) VALUES (%d,%d,1)",idSuperUser,idempresa);
+   conexionbase->ejecuta(query);
+
+   conexionbase->commit();
+   delete conexionbase;
+   delete this;
+}
