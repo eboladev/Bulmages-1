885e6395251c9eb8a314b8b3a8df2df8e498005a
Tomeu Borras <tborras@gmail.com>
2004-09-19 22:18:49 +0000

    *** empty log message ***

---
+++ bulmages/iglues/home.php
@@ -0,0 +1,62 @@
+<?php
+$title = '';
+$body = '';
+if (is_file($section = "contents/" . $_GET['go'] . ".inc")) {
+	include_once $section;
+} else if (is_file(($project = "projects/" . $_GET['prj'] . "/index.inc"))) {
+	include_once $project;
+} else {
+	if (! isset($_GET['go'])) {
+		include_once "contents/inicio.inc";
+	} else {
+		$title = "Sección en construcción";
+	};
+};
+
+?><!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+
+<head>
+  <title>IGLUES</title>
+  <meta name="GENERATOR" content="Quanta Plus">
+  <?php
+  ($stylesheet = $_GET['css']) || $stylesheet = "iglues.css";
+  echo "  <link rel=\"stylesheet\" type=\"text/css\" href=\"$stylesheet\">\n";
+  ?>
+</head>
+<?php
+echo "<body onLoad=\"easyinit()\">\n";
+?>
+<div id="header">
+<div id="section_title">
+<?php
+echo "<h1>$title</h1>";
+?>
+</div>
+</div>
+<div id="body">
+	<div id="navbar">
+	<div id="navbar_title"><div style="position:relative; top:.8em;">&nbsp;<strong>Contenidos:</strong></div></div>
+	<?php include "navbar.inc"; ?>
+	</div>
+
+
+
+
+	<div id="contents">
+	
+	<?php
+	include "navmenu.inc";
+	echo "<div id=\"contents_margins\">\n";
+
+	echo $body;
+	echo "\n<br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br /><br />\n";
+	?>
+	
+	</div>
+	
+	</div>
+</div>
+
+</body>
+</html>
