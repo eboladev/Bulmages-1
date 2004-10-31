4e4ea035808500dada0029eaf4e7f879e653c2a6
Tomeu Borras <tborras@gmail.com>
2004-10-31 19:55:55 +0000

    *** empty log message ***

---
+++ bulmages/installbulmages/dbmodels/crear/bulmafact/bulmafact_data.sql
@@ -0,0 +1,8 @@
+
+INSERT INTO division (descdivision) VALUES ('una division');
+INSERT INTO proveedor (iddivision, nomproveedor, cifproveedor, cpproveedor) VALUES (1,'Tomeu', '45678098D', '07008');
+INSERT INTO proveedor (iddivision, nomproveedor, cifproveedor, cpproveedor) VALUES (1,'Cristina', '45679998D', '07008');
+INSERT INTO proveedor (iddivision, nomproveedor, cifproveedor, cpproveedor) VALUES (1,'Marco Aurelio', '45668998D', '07008');
+INSERT INTO proveedor (iddivision, nomproveedor, cifproveedor, cpproveedor) VALUES (1,'Torrijas', '45678995D', '07008');
+ 
+
