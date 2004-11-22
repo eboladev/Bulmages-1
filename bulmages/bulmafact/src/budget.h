c60fef77d4ad2cdd249d91ada5ddcc224e3250c4
Tomeu Borras <tborras@gmail.com>
2004-11-22 17:09:53 +0000

    Presupuestos

---
+++ bulmages/bulmafact/src/budget.h
@@ -0,0 +1,42 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Tomeu Borrás Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+#ifndef BUDGET_H
+#define BUDGET_H
+
+// PRESUPUESTOS.
+
+#include "budgetbase.h"
+
+/** @author Tomeu Borrás Riera */
+class company;
+
+
+class Budget : public BudgetBase {
+Q_OBJECT
+private:
+   company *companyact;
+public:
+    Budget(company *emp, QWidget *parent = 0, const char *name = 0);
+    ~Budget();
+public slots:
+   virtual void close();
+};
+
+#endif
