d39cbd4bcf401d36b58145601ba4937bfadb15be
Tomeu Borras <tborras@gmail.com>
2005-02-14 19:02:18 +0000

    funcionalidad dockwindow

---
+++ bulmages/bulmafact/src/listventanas.cpp
@@ -0,0 +1,58 @@
+#include <listventanas.h>
+#include <qobject.h>
+
+#include <map>
+
+using namespace std;
+typedef QObject *pQObject;
+
+typedef map<int, pQObject> mapa;
+//typedef map<int, QString> mapa;
+
+mapa elmapa;
+
+listventanas::listventanas(QWidget *a, const char *b): QDockWindow (a,b) {
+	m_listBox = new QListBox( this ,0 );
+	m_listBox->setCaption( tr( "Ventanas Abiertas") );
+	setGeometry(100,100,100,500);
+	setFixedExtentWidth(200);
+	setWidget(m_listBox);
+	setResizeEnabled(TRUE);
+	setMovingEnabled(TRUE);
+	connect(m_listBox, SIGNAL(doubleClicked(QListBoxItem *)), this, SLOT(clicked()));
+}// end listventanas
+
+void listventanas::clicked() {
+	fprintf(stderr,"Se ha pulsado\n");
+	int item = m_listBox->currentItem();
+	QWidget *widget = (QWidget *)elmapa[item];
+	widget->hide();
+	widget->show();
+	widget->setActiveWindow();
+}// end clicked
+
+listventanas::~listventanas() {
+	delete m_listBox;
+}// end ~listventanas
+
+/** \brief This function provides the ability of push a window in the dock window
+ ** \param  nombre This QStrin is the name of the window that was shown in the listbox
+ ** \param obj This QObject * contains the pointer of the window for furtner reference.
+ */
+void  listventanas::meteWindow(QString nombre, QObject *obj) {
+	m_listBox->insertItem(nombre,-1);
+	elmapa[m_listBox->count()-1]=obj;
+}// end meteWindow
+
+
+void listventanas::sacaWindow(QObject *obj) {
+	mapa::iterator iterador = elmapa.begin();
+	while (iterador != elmapa.end()) {
+		if ((*iterador).second == obj) {
+			m_listBox->removeItem((*iterador).first);
+			elmapa.erase((*iterador).first);
+		}
+		iterador++;
+	}// end while
+}// end sacaWindow
+
---
+++ bulmages/bulmafact/src/listventanas.h
@@ -0,0 +1,27 @@
+// (C) Tomeu Borrás Riera, Febrero 2005
+
+#ifndef __LISTVENTANAS__
+#define __LISTVENTANAS__
+#include <qdockwindow.h>
+#include <qlistbox.h>
+
+
+/** \brief Provides the dock window to list all windows in BulmaFact
+ ** \param m_listBox this variable is the listBox that contains all titles of the diferent windows.
+*/
+class listventanas : public QDockWindow {
+Q_OBJECT
+	private:
+	QListBox *m_listBox ;
+	public:
+	listventanas(QWidget *, const char *);
+	~listventanas();
+	public:
+	void meteWindow(QString, QObject *);
+	void sacaWindow(QObject *);
+public slots:
+	virtual void clicked();
+};
+
+#endif
+
