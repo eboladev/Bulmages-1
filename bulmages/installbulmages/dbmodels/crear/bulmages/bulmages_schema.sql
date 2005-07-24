4ffd786802923ce0d831a2bf6a05f63651dfdb3c
gpg4all <gpg4all@users.berlios.de>
2005-07-24 17:30:05 +0000

    modificar para que lo haga todo el bulmages_schema y no compruebe diferentes lugares para diferentes distribuciones de linux

--- bulmages/installbulmages/dbmodels/crear/bulmages/bulmages_schema.sql
+++ bulmages/installbulmages/dbmodels/crear/bulmages/bulmages_schema.sql
@@ -1,3 +1,42 @@
+
+--
+-- PostgreSQL database definition of Bulmages
+-- Creamos el lenguaje plpgsql y lo preparamos para ser usado con toda la base
+-- de datos.
+
+SET client_encoding = 'SQL_ASCII';
+SET check_function_bodies = false;
+
+SET search_path = public, pg_catalog;
+
+--
+-- TOC entry 84 (OID 1345917)
+-- Name: plpgsql_call_handler(); Type: FUNC PROCEDURAL LANGUAGE; Schema: public; Owner: postgres
+--
+
+CREATE FUNCTION plpgsql_call_handler() RETURNS language_handler
+   AS '$libdir/plpgsql', 'plpgsql_call_handler'
+    LANGUAGE c;
+
+
+--
+-- TOC entry 83 (OID 1345918)
+-- Name: plpgsql; Type: PROCEDURAL LANGUAGE; Schema: public; Owner: 
+--
+
+CREATE TRUSTED PROCEDURAL LANGUAGE plpgsql HANDLER plpgsql_call_handler;
+
+
+--
+-- TOC entry 4 (OID 2200)
+-- Name: public; Type: ACL; Schema: -; Owner: postgres
+--
+
+REVOKE ALL ON SCHEMA public FROM PUBLIC;
+GRANT ALL ON SCHEMA public TO PUBLIC;
+
+
+
 -- La tabla de configuración.
 -- En esta tabla se guardan parametros que el programa va a utilizar.
 -- Como por ejemplo el numero de dígitos por defecto de las cuentas o el asiento inteligente que se enlaza con
