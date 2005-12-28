c4bc39ed06a833723da3d45aead9b39de77e2a10
Leopold Palomo Avellaneda <leo@alaxarxa.net>
2005-12-28 23:47:37 +0000

    Trunk convertido en la 0.5.3

--- bulmages/debian/debian/menu.ex
+++
@@ -1,2 +0,0 @@
-?package(bulmages):needs="X11|text|vc|wm" section="Apps/see-menu-manual"\
-  title="bulmages" command="/usr/bin/bulmages"
--- bulmages/introbulmages/src/comun/gongimportfiles.h
+++
@@ -1,4 +0,0 @@
-#include <qfile.h>
-
-int Contaplus2Fugit(QFile &fugitfile, QFile &subcuentas, QFile &asientos);
-
