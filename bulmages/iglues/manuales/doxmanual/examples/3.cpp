a807e1e082999c870e135c9ef03cf75e48a5d330
Leopold Palomo Avellaneda <leo@alaxarxa.net>
2005-12-29 00:24:34 +0000

    Un poco de limpieza. Estos directorios y ficheros ya estan en trunk

--- bulmages/iglues/manuales/doxmanual/examples/3.cpp
+++
@@ -1,7 +0,0 @@
-int a;///Esto es un comentario de la variable a
-int b;///Esto es un comentario de la variable b
-int c;///Esto es un comentario de la variable c
-
-int d;
-
-float x;///< Estoy comentando a \c x
