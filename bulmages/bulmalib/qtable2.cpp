e35f58bfb6ebfec1f4354e591a6dccd882008a0a
Tomeu Borras <tborras@gmail.com>
2006-03-11 14:59:23 +0000

    qtable2 para poder migrar a Qt4

---
+++ bulmages/bulmalib/qtable2.cpp
@@ -0,0 +1,193 @@
+/***************************************************************************
+                          qtable1.cpp  -  description
+                             -------------------
+    begin                : Mon Jan 13 2003
+    copyright            : (C) 2003 by Tomeu Borr� Riera
+    email                : tborras@conetxia.com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#include "qtable2.h"
+#include <qfont.h>
+#include <qpainter.h>
+#include <qcolor.h>
+#include <qdatetime.h>
+//Added by qt3to4:
+#include <QKeyEvent>
+#include <QEvent>
+
+
+#include "configuracion.h"
+#include "funcaux.h"
+
+QTableWidget2::QTableWidget2(QWidget * parent ):QTableWidget(parent ) {
+}
+
+/*
+void QTableWidget2::sortColumn ( int col, bool ascending, bool) {
+    int lastcol = numCols();
+
+    insertColumns(lastcol,3);
+    hideColumn(lastcol);
+    hideColumn(lastcol + 1);
+    hideColumn(lastcol + 2);
+     bool oknumero = TRUE;
+     bool okfecha = TRUE;
+
+    for (int x = 0; x < numRows(); x++) {
+		QString cad = text(x,col);
+		if (cad != "") {
+			setText(x,lastcol+0,cad);
+			/// Comprobamos si es un número.
+			cad.toDouble(&oknumero);
+			if (oknumero) {
+				while (cad.length() < 10)
+					cad.insert(0,"0");
+				setText(x,lastcol + 1,cad);
+			}// end if
+
+			if (okfecha) {
+				if (cad[2] == '/') {
+					QDate fech = normalizafecha(cad);
+					cad = fech.toString(Qt::ISODate);
+				} else {
+					okfecha = FALSE;
+				}// end if
+				setText(x,lastcol + 2,cad);
+			}// end if
+		}// end if
+
+    } // end for
+
+	if (oknumero)
+  		  Q3Table::sortColumn(lastcol+1,ascending,true);
+	else if (okfecha) 
+  		  Q3Table::sortColumn(lastcol+2,ascending,true);	
+	else 
+  		  Q3Table::sortColumn(lastcol+0,ascending,true);	
+
+    removeColumn(lastcol+2);
+    removeColumn(lastcol+1);
+    removeColumn(lastcol+0);
+}
+
+*/
+
+/*
+QWidget *QTableWidget2::beginEdit(int row, int col, bool type) {
+    return(Q3Table::beginEdit(row,col,type));
+}
+*/
+
+
+bool QTableWidget2::eventFilter( QObject *obj, QEvent *event ) {
+    _depura("QTableWidget2::INIT_eventFilter()\n",0);
+    static bool ctrlpulsado= FALSE;
+    if ( event->type() == QEvent::KeyPress ) {
+        QKeyEvent *keyEvent = (QKeyEvent *) event;
+        int key = keyEvent->key();
+
+        if ( key == Qt::Key_Plus) {
+            emit pulsadomas(currentRow(), currentColumn(), key);
+            return TRUE;
+        }// end if
+        if (key == Qt::Key_Asterisk) {
+            emit pulsadomas(currentRow(), currentColumn(), key);
+            return TRUE;
+        }// end if
+        if (key == Qt::Key_Enter || key == Qt::Key_Return) { // El enter
+            emit pulsadomas(currentRow(), currentColumn(), 4100);
+            return TRUE;
+        }// end if
+        if (key == 4115) {// La tecla hacia arriba
+            if (ctrlpulsado)   // Solo en combinacion con el ctrl
+                emit pulsadomas(currentRow(), currentColumn(), key);
+        }// end if
+        if (key == Qt::Key_Delete) {
+            if (ctrlpulsado)   // Solo en combinacion con el ctrl
+                emit pulsadomas(currentRow(), currentColumn(), key);
+        }// end if
+        if (key == 4117) {// La tecla hacia arriba
+            if (ctrlpulsado)   // Solo en combinacion con el ctrl
+                emit pulsadomas(currentRow(), currentColumn(), key);
+        }// end if
+        if (key == 4129) { // el Control
+            ctrlpulsado = TRUE;
+        }// end if
+	if (key == 47) {  // El dividir /
+	   emit pulsadomas(currentRow(), currentColumn(), key);
+	   return TRUE;
+	}// end if
+    }// end if
+    if (event->type() == QEvent::KeyRelease) {
+        QKeyEvent *keyEvent = (QKeyEvent *) event;
+        int key = keyEvent->key();
+        if (key == 4129) {
+            ctrlpulsado = FALSE;
+        }// end if
+    }// end if
+    _depura("QTableWidget2::END_eventFilter()\n",0);
+    return QTableWidget::eventFilter(obj, event);
+}// end eventFilter
+
+
+
+
+
+
+
+
+
+
+
+/*
+QTableItem1(QTable *table ,EditType et ,const QString &text, int mode): QTableItem(table, et, text) {
+  modo = mode;
+}// end if
+*/
+
+/*
+void QTableItem1::paint( QPainter *p, const QColorGroup &cg, const QRect &cr, bool selected ) {
+    QColorGroup g( cg );
+    // last row is the sum row - we want to make it more visible by
+    // using a red background
+    //    if ( row() == table()->numRows() - 1 )
+//    g.setColor( QColorGroup::Base, QColor::QColor(200,200,255) );
+    QFont f(p->font());
+    // Establecemos el color de fondo de este item como el color de fondo del diario.
+    g.setColor( QColorGroup::Base, QColor::QColor(confpr->valor(CONF_BG_DIARIO).toAscii().data()) );
+
+    // Establecemos la fuente segun las preferencias del diario.
+    f.setPointSize(atoi(confpr->valor(CONF_FONTSIZE_DIARIO).toAscii().data()));
+	 f.setFamily(confpr->valor(CONF_FONTFAMILY_DIARIO).toAscii().data());
+    p->setFont( f );
+
+    if (modo == 1) {
+        g.setColor( QColorGroup::Text, QColor::QColor(confpr->valor(CONF_FG_DIARIO1).toAscii().data()));
+    } else {
+        g.setColor(QColorGroup::Text, QColor::QColor(confpr->valor(CONF_FG_DIARIO2).toAscii().data()));
+    }// end if
+    
+    // MODO 10
+    if (modo == 10) {
+      g.setColor( QColorGroup::Base, QColor::QColor("#FFFFFF") );
+      // Establecemos la fuente segun las preferencias del diario.
+      f.setPointSize(atoi(confpr->valor(CONF_FONTSIZE_DIARIO).toAscii().data()));
+            f.setFamily(confpr->valor(CONF_FONTFAMILY_DIARIO).toAscii().data());
+      p->setFont( f );
+      g.setColor(QColorGroup::Text, QColor::QColor("#FF0000"));
+    }// end if
+    // FIN DEL MODO 10
+    
+    Q3TableItem::paint( p, g, cr, selected );
+}
+
+*/
+
---
+++ bulmages/bulmalib/qtable2.h
@@ -0,0 +1,61 @@
+/***************************************************************************
+                          qtable2.h  -  description
+                             -------------------
+    begin                : Mon Jan 13 2003
+    copyright            : (C) 2003 by Tomeu Borr� Riera
+    email                : tborras@conetxia.com
+ ***************************************************************************/
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef QTABLE2_H
+#define QTABLE2_H
+
+#include <q3table.h>
+//Added by qt3to4:
+#include <QEvent>
+#include <QTableWidget>
+
+/** *@author Tomeu Borrás Riera */
+
+
+class QTableWidgetItem2 : public QTableWidgetItem {
+public:
+   int modo;
+public:
+QTableWidgetItem2(int type = Type, int mode=0): QTableWidgetItem(type) {modo=mode;}
+QTableWidgetItem2(const QString & text, int type = Type, int mode=0): QTableWidgetItem(text, type) {modo=mode;}
+};
+
+
+
+class QTableWidget2 : public QTableWidget{
+  Q_OBJECT
+public:
+   int modo; // Indica el modo de dibujo.
+public:
+  QTableWidget2(QWidget * parent = 0 );
+//  void sortColumn ( int col, bool ascending = TRUE, bool wholeRows = TRUE );
+//  QWidget *beginEdit (int, int, bool);
+   virtual bool eventFilter( QObject *obj, QEvent *event );
+//   virtual void setItem(int, int, QTableWidgetItem2 *);
+//   void  paintCell ( QPainter * , int , int , const QRect &, bool  );
+
+	void setText( int x, int y, const QString & val) {
+			QTableWidgetItem2 *newitem = new QTableWidgetItem2(val);
+		setItem(x, y, newitem);
+	}// end setText
+
+
+signals:
+   void pulsadomas(int, int, int);  
+};
+
+#endif
+
