df965defe667a7e72de5bc015ba53f24c1acdc6e
Fco. Javier M.C <fcojavmc@todo-redes.com>
2006-06-05 20:01:51 +0000

    cambios

--- bulmages/bulmafact/src/plugin/plugin.cpp
+++ bulmages/bulmafact/src/plugin/plugin.cpp
@@ -1,48 +1,67 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <Q3PopupMenu>
+#include <QAction>
+#include <QObject>
+#include <QMessageBox>
+
 #include <stdio.h>
+
 #include "plugin.h"
 #include "company.h"
 #include "funcaux.h"
 
-#include <q3popupmenu.h>
-#include <qaction.h>
-#include <qobject.h>
-#include <qmessagebox.h>
-
-
 
 myplugin:: myplugin() {}
+
 myplugin::~myplugin() {}
 
 
 void myplugin::elslot() {
     fprintf(stderr,"SE ha activado el slot\n");
-    QMessageBox::warning( 0, "Guardar Familia",
-		"Desea guardar los cambios.",
-		QMessageBox::Ok ,
-		QMessageBox::Cancel );
-}// end elslot
-
+    QMessageBox::warning(0,
+                         "Guardar Familia",
+                         "Desea guardar los cambios.",
+                         QMessageBox::Ok,
+                         QMessageBox::Cancel);
+}
 
 
 void myplugin::inicializa(bulmafact *bges) {
-    //El menu de empresa
+    /// El menu de empresa.
     Q3PopupMenu *pPluginMenu = new Q3PopupMenu();
     pPluginMenu->setCheckable(true);
-    bges->menuBar()->insertItem("&Plugin",pPluginMenu);
+    bges->menuBar()->insertItem("&Plugin", pPluginMenu);
     QAction *planCuentas = new QAction("&Prueba de Plugin", 0);
     planCuentas->setStatusTip("Muestra el plan contable");
     planCuentas->setWhatsThis("Muestra el plan contable");
     planCuentas->addTo(pPluginMenu);
     connect(planCuentas, SIGNAL(activated()), this, SLOT(elslot()));
-}// end inicializa
-
+}
 
 
 void entryPoint(bulmafact *bges) {
-    _depura("Estoy dentro del plugin\n",0);
-    myplugin *plug= new myplugin( );
+    _depura("Estoy dentro del plugin\n", 0);
+    myplugin *plug = new myplugin();
     plug->inicializa(bges);
     bges->setCaption("Prueba de plugin.");
 }
-
 
--- bulmages/bulmafact/src/plugin/plugin.h
+++ bulmages/bulmafact/src/plugin/plugin.h
@@ -1,34 +1,57 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
 #ifdef Q_WS_WIN
 # define MY_EXPORT __declspec(dllexport)
 #else
 # define MY_EXPORT
 #endif
 
+#include <QStringList>
+#include <QWidget>
+#include <QIcon>
+#include <QApplication>
+#include <QObject>
 
 #include "bulmafact.h"
 #include "postgresiface2.h"
 
-#include <qstringlist.h>
-#include <qwidget.h>
-#include <qicon.h>
-#include <qapplication.h>
-#include <qobject.h>
 
 extern "C" MY_EXPORT void entryPoint(bulmafact *);
 
 extern QApplication *theApp;
 
 
-//class QT_WIDGET_PLUGIN_EXPORT myplugin : public QObject {
+/// class QT_WIDGET_PLUGIN_EXPORT myplugin : public QObject {
 class myplugin : public QObject {
-Q_OBJECT
+    Q_OBJECT
+
 public:
-  postgresiface2 *conexionbase;
+    postgresiface2 *conexionbase;
+
 public:
-   myplugin();
-   ~myplugin();
-       void inicializa(bulmafact *);
+    myplugin();
+    ~myplugin();
+    void inicializa(bulmafact *);
+
 public slots:
-	void elslot();
+    void elslot();
 };
 
