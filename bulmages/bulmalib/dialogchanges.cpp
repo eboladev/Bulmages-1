8c49ce98b75b48bb5b4c82240088a4bc01ea8746
Alvaro de Miguel <alvaro.demiguel@gmail.com>
2006-06-18 01:50:08 +0000

    Mejoras en las funciones de deteccion de cambios de los campos de la pantalla.

--- bulmages/bulmalib/dialogchanges.cpp
+++ bulmages/bulmalib/dialogchanges.cpp
@@ -2,59 +2,130 @@
 
 #include <QLineEdit>
 #include <QTextEdit>
+#include <QTableWidget>
 
 #include "dialogchanges.h"
 #include "funcaux.h"
 
 
 void dialogChanges::dialogChanges_cargaInicial() {
+	_depura("dialogChanges::dialogChanges_cargaInicial",0);
+	m_maxQText=0;
+	m_maxQLine=0;
+	m_maxQTable=0;
+
+/* ASI ES CON QT4 */
+    QList<QTextEdit *> l1 = m_obje->findChildren<QTextEdit *>();
+    QListIterator<QTextEdit *> it1 (l1);
+    while ( it1.hasNext() ) {
+        m_listaQText[m_maxQText++] = it1.next();
+	}
+
+    QList<QLineEdit *> l2 = m_obje->findChildren<QLineEdit *>();
+    QListIterator<QLineEdit *> it2 (l2);
+    while ( it2.hasNext() ) {
+        m_listaQLine[m_maxQLine++] = it2.next();
+	}
+
+    QList<QTableWidget *> l3 = m_obje->findChildren<QTableWidget *>();
+    QListIterator<QTableWidget *> it3 (l3);
+    while ( it3.hasNext() ) {
+        m_listaQTable[m_maxQTable++] = it3.next();
+	}
+
+/* ASI ERA CON QT3 */
+
+/*
+	QObjectList l = m_obje->queryList("QTextEdit");
+    QListIterator<QObject *> it1 (l);
+    while ( it1.hasNext() ) {
+        listaQText[maxQText++] = it1.next();
+	}
+
+	QObjectList l = m_obje->queryList("QLineEdit");
+    QListIterator<QObject *> it2 (l);
+    while ( it2.hasNext() ) {
+        m_listaQLine[m_maxQLine++] = it2.next();
+	}
+
+	l = m_obje->queryList("QTableWidget");
+    QListIterator<QObject *> it3 (l);
+    while ( it3.hasNext() ) {
+        m_listaQTable[m_maxQTable++] = it3.next();
+	}
+*/
+
     valorinicial = calculateValues();
+    _depura(valorinicial.toAscii(),1);
+	_depura("END dialogChanges::dialogChanges_cargaInicial",0);
 }
 
 
 bool dialogChanges::dialogChanges_hayCambios() {
+	_depura("dialogChanges::dialogChanges_hayCambios",0);
+    QString valorfinal = calculateValues();
+    _depura(valorfinal.toAscii(),0);
+	_depura(valorinicial.toAscii(),0);
+	qDebug(valorinicial.toAscii());
+	qDebug(valorfinal.toAscii());
+	_depura("END dialogChanges::dialogChanges_hayCambios",0);
     return !(valorinicial == calculateValues());
 }
 
 
 QString dialogChanges::calculateValues() {
     _depura("dialogChanges::calculateValues",0);
-    QString values = retrieveValues("QTable");
-    values += retrieveValues("QLineEdit");
-    values += retrieveValues("QTextEdit");
+    QString values = retrieveValues("QTableWidget");
+/*    values += retrieveValues("QLineEdit");
+    values += retrieveValues("QTextEdit"); */
     _depura("END dialogChanges::calculateValues",0);
     return values;
 }
 
 QString dialogChanges::retrieveValues(QString qsWidget) {
     _depura("dialogChanges::retrieveValues",0);
-    QObjectList l = obje->queryList( qsWidget );
+	QString values="";
+	for (int i = 0; i<m_maxQLine; i++) {
+		if (m_listaQLine[i]!=NULL)
+		values += ((QLineEdit*)m_listaQLine[i])->text();
+	}
+
+	for (int i = 0; i<m_maxQText; i++) {
+		if (m_listaQText[i]!=NULL)
+		values += ((QTextEdit*)m_listaQText[i])->text();
+	}
+/*    QObjectList l = obje->queryList( qsWidget );
     QListIterator<QObject *> it (l);
     QObject *obj;
     QString values="";
     while ( it.hasNext() ) {
         obj = it.next();
-        //		++it;
-        if (qsWidget=="QLineEdit") {
-            //qDebug("QLineEdit Name = %s",obj->name());
-            values += ((QLineEdit*)obj)->text();
-        }
+		if (obj->objectName()!="mui_query") {
+		_depura(obj->objectName(),0);
+		_depura(qsWidget,0);
+       	 //		++it;
+       	 if (qsWidget=="QLineEdit") {
+       	     //qDebug("QLineEdit Name = %s",obj->name());
+       	     values += ((QLineEdit*)obj)->text();
+       	 }
 
-        if (qsWidget=="QTextEdit") {
-            //qDebug("QTextEdit Name = %s",obj->name());
-            values += ((QTextEdit*)obj)->text();
-        }
+       	 if (qsWidget=="QTextEdit") {
+       	     //qDebug("QTextEdit Name = %s",obj->name());
+       	     values += ((QTextEdit*)obj)->text();
+       	 }
 
-        /*		if (qsWidget=="QTable") {
-        			//qDebug("QTable Name = %s",obj->name());
-        			for (int i=0; i < m_list->numRows(); i++) {
-        				for (int j=0; j < m_list->numCols(); j++) {
-        					values += ((QTable*)obj)->text(i, j);
-        				}
-        			}
-        		}
-        */
+		if (qsWidget=="QTableWidget") {
+       	    // values += ((QTableWidgetItem*)obj)->text();
+			_depura(obj->objectName(),0);
+			for (int i=0; i < ((QTableWidget*)obj)->rowCount(); i++) {
+				for (int j=0; j < ((QTableWidget*)obj)->columnCount(); j++) {
+					values += (((QTableWidget*)obj))->item(i, j)->text();
+				}
+			}
+       	 }
+		}
     }
+*/
     //	delete l; // delete the list, not the objects
     _depura("END dialogChanges::retrieveValues",0);
     return values;
--- bulmages/bulmalib/dialogchanges.h
+++ bulmages/bulmalib/dialogchanges.h
@@ -1,6 +1,11 @@
 #ifndef __DIALOGCHANGES__
 #define __DIALOGCHANGES__
 
+/* #define NUMQTEXT = 999
+#define NUMQLINE = 999
+#define NUMQTABLE = 99999
+*/
+#include "funcaux.h"
 #include <QObject>
 
 /** Esta clase se encarga de proporcionar algo de infraestructura al control de cambios
@@ -9,11 +14,24 @@
   * Luego para saber si hay cambios en el formulario vuelve a comparar.
   */
 class dialogChanges {
+
 private:
+	int m_maxQLine;
+	int m_maxQText;
+	int m_maxQTable;
 	QString valorinicial;
-	QObject *obje;
+	QObject *m_obje;
+	QObject *m_listaQLine[999];
+	QObject *m_listaQText[999];
+	QObject *m_listaQTable[999];
+
 public:
-	dialogChanges(QObject *ob) {obje=ob;};
+	dialogChanges(QObject *ob) {
+_depura("dialogChanges::dialogChanges",0);
+m_obje=ob;
+_depura("END dialogChanges::dialogChanges",0);
+
+};
 	~dialogChanges(){};
 	void dialogChanges_cargaInicial();
 	bool dialogChanges_hayCambios();
