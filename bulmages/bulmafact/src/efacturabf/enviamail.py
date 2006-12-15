dcb6dd7afa7f58cf4d55a8783e216e1a7ae123fb
Arturo Martín Llado <antar_cheran@yahoo.es>
2006-12-15 17:49:17 +0000

    Creado el script de recepcion de emails. Para cada e-mail separa mensaje de archivos adjuntos en un directorio aparte. Reindentacion del codigo del script de enviar e-mails (sustituidos espacios por tabuladores).

--- bulmages/bulmafact/src/efacturabf/enviamail.py
+++ bulmages/bulmafact/src/efacturabf/enviamail.py
@@ -9,28 +9,28 @@ from email import Encoders
 
 def enviaEmail(origen, destino, asunto, text, archivo, server="localhost"):
 
-    msg = MIMEMultipart()
-    msg['From'] = origen
-    msg['To'] = COMMASPACE.join(destino)
-    msg['Date'] = formatdate(localtime=True)
-    msg['Subject'] = asunto
-
-    msg.attach( MIMEText(text) )
-
-    part = MIMEBase('application', "octet-stream")
-    part.set_payload( open(archivo,"rb").read() )
-    Encoders.encode_base64(part)
-    part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(archivo))
-    msg.attach(part)
-
-    smtp = smtplib.SMTP(server)
-    smtp.sendmail(origen, destino, msg.as_string() )
-    smtp.close()
+	msg = MIMEMultipart()
+	msg['From'] = origen
+	msg['To'] = COMMASPACE.join(destino)
+	msg['Date'] = formatdate(localtime=True)
+	msg['Subject'] = asunto
+	
+	msg.attach( MIMEText(text) )
+	
+	part = MIMEBase('application', "octet-stream")
+	part.set_payload( open(archivo,"rb").read() )
+	Encoders.encode_base64(part)
+	part.add_header('Content-Disposition', 'attachment; filename="%s"' % os.path.basename(archivo))
+	msg.attach(part)
+	
+	smtp = smtplib.SMTP(server)
+	smtp.sendmail(origen, destino, msg.as_string())
+	smtp.close()
 
 enviaEmail (
-    sys.argv[1], # Origen
-    sys.argv[2], # Destino
-    sys.argv[3], # Asunto, pasado como argumento entre comillas dobles
-    sys.argv[4], # Mensaje, pasado como argumento entre comillas dobles
-    sys.argv[5]  # Archivo adjunto, en este caso la efactura
+	sys.argv[1], # Origen
+	sys.argv[2], # Destino
+	sys.argv[3], # Asunto, pasado como argumento entre comillas dobles
+	sys.argv[4], # Mensaje, pasado como argumento entre comillas dobles
+	sys.argv[5]  # Archivo adjunto
 )
