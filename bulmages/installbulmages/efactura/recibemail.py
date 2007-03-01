d8968ff1b46e0f97183d57fb74324acb4abd8b09
Arturo Martín Llado <antar_cheran@yahoo.es>
2007-03-01 19:24:12 +0000

    Ahora el script solo presta atencion a los archivos XML que contenga un e-mail. Se puede hacer generico para otros usos, pero lo he hecho ad-hoc debido a que solo pretendo guardar las efacturas (archivos XML) que se reciban por e-mail.
    Lo nuevo que he incorporado era lo anunciado. Ahora se genera un HASH MD5 del contenido de la efactura y se crea un directorio con ese nombre. La efactura se guarda en dicho directorio, asi no tenemos problemas a la hora de automatizar los guardados y, si mandan la misma efactura en otros emails, se sobreescribira en su correspondiente directorio. Esto tambien ayudara en adelante en el proceso de obtencion del listado de directorios del directorio configurado para guardar las efacturas. Asi se pueden mostrar y administrar en bulmafact comodamente.
    
    Esto ultimo es lo siguiente que hay por hacer. La administracion de los archivos obtenidos.

--- bulmages/installbulmages/efactura/recibemail.py
+++ bulmages/installbulmages/efactura/recibemail.py
@@ -4,28 +4,24 @@ import email
 import errno
 import mimetypes
 import poplib
+import md5
+import string
 
-def desempaquetaEmail(archivo, directorio):
-	
-	if not directorio:
-		print "Error, no se ha pasado como parametro el nombre del directorio donde se desempaquetaran los correos"
-		sys.exit(1)
+def desempaquetaEmail(archivo):
 	
 	if not archivo:
 		print "Error, no se ha pasado como parametro el nombre del archivo que contiene el e-mail a desempaquetar"
 		sys.exit(1)
-
-	try:
-		os.mkdir(directorio)
-	except OSError, e:
-		# Ignoramos el error de directorio ya existente
-		if e.errno <> errno.EEXIST:
-			raise
 	
+	# Abrimos el archivo y lo obtenemos como mensaje de correo electronico dentro de msg
 	fp = open(archivo)
 	msg = email.message_from_file(fp)
 	fp.close()
 	
+	# Instancia del objeto MD5 que nos servira para crear los nombres de los directorios con el HASH del XML
+	m = md5.new()
+	
+	# Empezamos a desempaquetar el mensaje. Ignoramos todo lo que no sean XMLs
 	contador = 1
 	for part in msg.walk():
 		if part.get_content_maintype() == 'multipart':
@@ -34,18 +30,49 @@ def desempaquetaEmail(archivo, directorio):
 		nomarchivo = part.get_filename()
 		extension = mimetypes.guess_extension(part.get_content_type())
 		
-		if not extension:
-			extension = '.email'
+		#if not extension:
+			#extension = '.email'
 
+		# Para el mensaje del email, que no devuelve nombre de archivo y hace que
+		# esto pete en el if para encontrar la cadena '.xml'
 		if not nomarchivo:
 			nomarchivo = 'part-%03d%s' % (contador, extension)
 		
 		contador += 1
-		fp = open(os.path.join(directorio, nomarchivo), 'wb')
-		fp.write(part.get_payload(decode=True))
-		fp.close()
+		
+		# Si es un XML suponemos que es una e-factura y generamos un HASH MD5 con el contenido del archivo.
+		# Este HASH sera el nombre del directorio donde alojemos finalmente el XML. Asi evitamos el problema
+		# de como nombrar a un directorio una vez obtenida la factura.
+		# Usamos esto y no damos lugar a la imaginacion, ya nos da una manera segura para no repetir nombres.
+		
+		
+		#encontrado = nomarchivo.find('.xml')
+		
+		if nomarchivo.find('.xml') <> -1:
+			# Aqui se crea el HASH MD5 con el contenido del fichero XML
+			m.update(part.get_payload(decode=True))
+			
+			try:
+				# Creamos el directorio usando el HASH como nombre
+				os.mkdir(m.hexdigest())
+			except OSError, e:
+				# Ignoramos el error de directorio ya existente (nos mandan la misma efactura varias veces)
+				if e.errno <> errno.EEXIST:
+					raise
+			
+			# Vamos al directorio que acabamos de crear para escribir ahi el fichero XML
+			os.chdir(m.hexdigest())
+			
+			fp = open(nomarchivo, 'wb')
+			fp.write(part.get_payload(decode=True))
+			fp.close()
+			
+			# Volvemos al anterior directorio de trabajo para seguir con mas efacturas o acabar :)
+			os.chdir('../');
 
-def recibeEmail(servidor, usuario, password, directorio="diremail", archivo="email"):
+def recibeEmail(servidor, usuario, password, archivo="email"):
+
+	# El parametro archivo solo es el prefijo con el que va el archivo temporal usado para desempaquetar
 
 	M = poplib.POP3(servidor)
 	M.user(usuario)
@@ -74,10 +101,13 @@ def recibeEmail(servidor, usuario, password, directorio="diremail", archivo="ema
 		f.write(string)
 		f.close()
 
-		# Dividimos el archivo en mensajes y adjunto/s
-		desempaquetaEmail(archivo+numero, directorio+numero)
+		# Obtenemos el XML de la efactura
+		desempaquetaEmail(archivo+numero)
 
-		# Obtenemos y decimos que se borre el mensaje al cerrar la conexion ( M.quit() )
+		# Ahora toca borrar el fichero del cual hemos desempaquetado el XML
+		os.remove(archivo+numero)
+
+		# Indicamos que se borre el mensaje del servidor al cerrar la conexion ( con M.quit() )
 		M.dele(i+1)
 
 	#print "Mensajes nuevos:"
