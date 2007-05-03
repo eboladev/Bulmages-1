bcd712df1853c49b96421eba4749c049591f4dd2
Tomeu Borras <tborras@gmail.com>
2007-05-03 18:28:40 +0000

    Se me olvido subir estos dos archivos al repositorios. Son el selector de bancos en modo de comboBox.

---
+++ bulmages/bulmafact/src/busquedabanco.cpp
@@ -0,0 +1,104 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QComboBox>
+
+#include "busquedabanco.h"
+#include "company.h"
+
+
+/** Inicializa todos los componentes a NULL para que no haya confusiones sobre
+    si un elemento ha sido inicializado o no.
+    Hace la conexion del SIGNAL activated con m_activated para tratar el evento.
+*/
+BusquedaBanco::BusquedaBanco(QWidget *parent)
+        : QComboBox(parent) {
+    _depura("BusquedaBanco::BusquedaBanco", 0);
+    companyact = NULL;
+    m_cursorcombo = NULL;
+    connect(this, SIGNAL(activated(int)), this, SLOT(m_activated(int)));
+    _depura("END BusquedaBanco::BusquedaBanco", 0);
+}
+
+
+/** Libera la memoria dinamica que se estaba utilizando.
+*/
+BusquedaBanco::~BusquedaBanco() {
+    _depura("BusquedaBanco::~BusquedaBanco", 0);
+    if (m_cursorcombo != NULL)
+        delete m_cursorcombo;
+    _depura("END BusquedaBanco::~BusquedaBanco", 0);
+}
+
+
+/** Con este metodo se puede indicar al Widget cual es el banco seleccionado.
+    Recarga el cursor de bancoes y cuando encuentra uno con el mismo identificador
+    que el que se ha pasado por parametro lo establece como elemento seleccionado.
+*/
+void BusquedaBanco::setidbanco(QString idbanco) {
+    _depura("BusquedaBanco::setidbanco", 0);
+    if (m_cursorcombo != NULL)
+        delete m_cursorcombo;
+    m_cursorcombo = companyact->cargacursor("SELECT * FROM banco");
+    int i = 0;
+    int i1 = 0;
+    clear();
+    addItem("--");
+    while (!m_cursorcombo->eof()) {
+        i ++;
+        if (m_cursorcombo->valor("idbanco") == idbanco)
+            i1 = i;
+        addItem(m_cursorcombo->valor("nombanco") );
+        m_cursorcombo->siguienteregistro();
+    } // end while
+    setCurrentIndex(i1);
+    _depura("END BusquedaBanco::setidbanco", 0);
+}
+
+
+/** Inicializa la clase con el puntero a la empresa que se esta utilizando.
+**/
+void BusquedaBanco::setcompany(company *comp) {
+    _depura("BusquedaBanco::setcompany", 0);
+    companyact = comp;
+    _depura("END BusquedaBanco::setcompany", 0);
+}
+
+/** Devuelve el identificador del banco seleccionado
+**/
+QString BusquedaBanco::idbanco() {
+    _depura("BusquedaBanco::idbanco", 0);
+    _depura("END BusquedaBanco::idbanco", 0);
+    return m_cursorcombo->valor("idbanco", currentIndex() - 1);
+}
+
+/** SLOT que responde a la activacion de un elemento en el QComboBox y que hace que se emita el SIGNAL valueChanged
+**/
+void BusquedaBanco::m_activated(int index) {
+    _depura("BusquedaBanco::m_activated", 0);
+    if (index > 0) {
+        emit(valueChanged(m_cursorcombo->valor("idbanco", index - 1)));
+    } else {
+        emit(valueChanged(""));
+    }
+    _depura("END BusquedaBanco::m_activated", 0);
+}
+
+
---
+++ bulmages/bulmafact/src/busquedabanco.h
@@ -0,0 +1,61 @@
+/***************************************************************************
+ *   Copyright (C) 2007 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef BUSQUEDABANCO_H
+#define BUSQUEDABANCO_H
+
+#include <QComboBox>
+
+#include "company.h"
+#include "postgresiface2.h"
+#include "funcaux.h"
+
+
+/// Permite buscar y seleccionar un trabajador.
+/** Este comboBox permite buscar y seleccionar un
+    trabajador. Aparece en forma de desplegable.
+    Antes de utilizarlo debe ser inicializado con setcompany().
+    Cuando se cambia el valor del Widget se emite un SIGNAL valueChanged().
+*/
+class BusquedaBanco : public QComboBox {
+    Q_OBJECT
+
+private:
+    /// Puntero a la clase company para poder trabajar con la Base de datos y poder hacer paso de mensajes.
+    company *companyact;
+    /// Cursor que almacena el listado de trabajadores y sobre el que se construye el comboBox.
+    cursor2 *m_cursorcombo;
+
+public:
+    BusquedaBanco(QWidget *parent = 0);
+    ~BusquedaBanco();
+    void setcompany(company *comp);
+    virtual void setidbanco(QString idbanco);
+    QString idbanco();
+
+public slots:
+    void m_activated(int index);
+
+signals:
+    void valueChanged(QString);
+};
+
+#endif
+
