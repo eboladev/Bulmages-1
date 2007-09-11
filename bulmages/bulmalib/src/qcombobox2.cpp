53f8609f4db2106c67741283db9489af26654d31
Tomeu Borras <tborras@gmail.com>
2007-09-11 07:07:00 +0000

    Me faltaban estos dos archivos por subir.

---
+++ bulmages/bulmalib/src/qcombobox2.cpp
@@ -0,0 +1,21 @@
+
+#include "qcombobox2.h"
+
+/** Inicializa todos los componenetes a NULL para que no se produzcan confusiones
+    sobre si un elemento ha sido creado o no.
+    Conecta el activar un tipo con m_activated.
+*/
+/// \TODO: No deberia usarse m_activated como signal ya que confunde con una variable de clase.
+QComboBox2::QComboBox2(QWidget *parent)
+        : QComboBox(parent), PEmpresaBase() {
+
+}
+
+
+/** Se libera memoria destruyendo el cursor que almacena los tipos de IVA si es que esta creado.
+*/
+QComboBox2::~QComboBox2() {
+
+}
+
+
