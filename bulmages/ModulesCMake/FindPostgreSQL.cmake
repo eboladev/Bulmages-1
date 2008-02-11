abcc179e28b1eb46b6b43d0c26ec41a3cceb15f5
Leopold Palomo Avellaneda <leo@alaxarxa.net>
2008-02-11 19:42:12 +0000

    Actualizados los CMakeList poniendoles cabecera indicando autoria. (Tomeu et faig cas ...)

--- bulmages/ModulesCMake/FindPostgreSQL.cmake
+++ bulmages/ModulesCMake/FindPostgreSQL.cmake
@@ -1,12 +1,36 @@
-# - Find libpq
-# Find the native PostgreSQL includes and library
-#
+# 
+# FindPostgresSQL.cmake
+# 
+# Find the native PostgreSQL includes and library (libpq)
+# Defines:
 #  PGSQL_INCLUDE_DIR - where to find libpq-fe.h, etc.
 #  PGSQL_LIBRARIES   - list of libraries when using PGSQL.
-#  PGSQL_BIN_DIR	 - where are the pg utils found
+#  PGSQL_BIN_DIR     - where are the pg utils found
 #  PGSQL_FOUND       - True if PGSQL found.
 # from the pvpgn GPL license http://pvpgn.berlios.de/
 # ${PGSQL_LIBRARIES})
+#
+# Modified and adapted by Leopold Palomo-Avellaneda 
+#
+# Copyright (C) 2008                                                    #
+# leo@alaxarxa.net                                                      #
+# http://www.iglues.org                                                 #
+#                                                                       #
+# This program is free software; you can redistribute it and/or modify  #
+# it under the terms of the GNU General Public License as published by  #
+# the Free Software Foundation; either version 2 of the License, or     #
+# (at your option) any later version.                                   #
+#                                                                       #
+# This program is distributed in the hope that it will be useful,       #
+# but WITHOUT ANY WARRANTY; without even the implied warranty of        #
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         #
+# GNU General Public License for more details.                          #
+#                                                                       #
+# You should have received a copy of the GNU General Public License     #
+# along with this program; if not, write to the                         #
+# Free Software Foundation, Inc.,                                       #
+# 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             #
+#                                                                       #
 
 FIND_PROGRAM(CMAKE_PGCONFIG_EXECUTABLE pg_config)
 
