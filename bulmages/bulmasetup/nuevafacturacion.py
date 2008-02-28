d04f4bd3a2053dce26168ac1e6f5c83431457ba8
Tomeu Borras <tborras@gmail.com>
2008-02-28 21:29:54 +0000

    Arreglo unas cosillas de bulmasetup y empiezo a usar las llamadas de sistema

--- bulmages/bulmasetup/nuevafacturacion.py
+++ bulmages/bulmasetup/nuevafacturacion.py
@@ -1,4 +1,5 @@
 import sys
+import os
 from PyQt4 import *
 from nuevafacturacionbase import *
 
@@ -9,6 +10,11 @@ class NuevaFacturacion(QtGui.QDialog, Ui_NuevaFacturacionBase):
         QtGui.QDialog.__init__(self,parent)
 	self.setupUi(self)
 	
+    def on_mui_botonera_accepted(self):
+	self.nomdb = self.mui_nomdb.text()
+	self.command = 'su postgres -c createdb ' + self.nomdb
+	QtGui.QMessageBox.warning(self, "My Application", self.command, QtGui.QMessageBox.Save | QtGui.QMessageBox.Discard | QtGui.QMessageBox.Cancel, QtGui.QMessageBox.Save)
+#        os.system(command)
 
 
 def main(args):
--- bulmages/bulmasetup/nuevafacturacionbase.py
+++ bulmages/bulmasetup/nuevafacturacionbase.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'nuevafacturacionbase.ui'
 #
-# Created: Tue Feb 26 10:41:15 2008
+# Created: Thu Feb 28 21:25:47 2008
 #      by: PyQt4 UI code generator 4.1
 #
 # WARNING! All changes made in this file will be lost!
@@ -23,40 +23,52 @@ class Ui_NuevaFacturacionBase(object):
         NuevaFacturacionBase.setMaximumSize(QtCore.QSize(578,608))
         NuevaFacturacionBase.setWindowIcon(QtGui.QIcon("../images/png/bulmafact.png"))
 
-        self.lineEdit = QtGui.QLineEdit(NuevaFacturacionBase)
-        self.lineEdit.setGeometry(QtCore.QRect(290,30,221,25))
-        self.lineEdit.setObjectName("lineEdit")
+        self.gridlayout = QtGui.QGridLayout(NuevaFacturacionBase)
+        self.gridlayout.setMargin(9)
+        self.gridlayout.setSpacing(6)
+        self.gridlayout.setObjectName("gridlayout")
 
-        self.label = QtGui.QLabel(NuevaFacturacionBase)
-        self.label.setGeometry(QtCore.QRect(106,30,141,20))
-        self.label.setObjectName("label")
-
-        self.label_2 = QtGui.QLabel(NuevaFacturacionBase)
-        self.label_2.setGeometry(QtCore.QRect(110,60,161,20))
-        self.label_2.setObjectName("label_2")
-
-        self.lineEdit_2 = QtGui.QLineEdit(NuevaFacturacionBase)
-        self.lineEdit_2.setGeometry(QtCore.QRect(290,60,221,25))
-        self.lineEdit_2.setObjectName("lineEdit_2")
-
-        self.buttonBox = QtGui.QDialogButtonBox(NuevaFacturacionBase)
-        self.buttonBox.setGeometry(QtCore.QRect(230,560,341,32))
-        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.NoButton|QtGui.QDialogButtonBox.Ok)
-        self.buttonBox.setObjectName("buttonBox")
+        self.mui_botonera = QtGui.QDialogButtonBox(NuevaFacturacionBase)
+        self.mui_botonera.setOrientation(QtCore.Qt.Horizontal)
+        self.mui_botonera.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.NoButton|QtGui.QDialogButtonBox.Ok)
+        self.mui_botonera.setObjectName("mui_botonera")
+        self.gridlayout.addWidget(self.mui_botonera,3,0,1,2)
 
         self.groupBox = QtGui.QGroupBox(NuevaFacturacionBase)
-        self.groupBox.setGeometry(QtCore.QRect(110,170,441,291))
         self.groupBox.setObjectName("groupBox")
 
+        self.mui_contratos = QtGui.QCheckBox(self.groupBox)
+        self.mui_contratos.setGeometry(QtCore.QRect(30,30,161,19))
+        self.mui_contratos.setObjectName("mui_contratos")
+        self.gridlayout.addWidget(self.groupBox,2,0,1,2)
+
+        self.mui_nomdb = QtGui.QLineEdit(NuevaFacturacionBase)
+        self.mui_nomdb.setObjectName("mui_nomdb")
+        self.gridlayout.addWidget(self.mui_nomdb,1,1,1,1)
+
+        self.label_2 = QtGui.QLabel(NuevaFacturacionBase)
+        self.label_2.setObjectName("label_2")
+        self.gridlayout.addWidget(self.label_2,1,0,1,1)
+
+        self.label = QtGui.QLabel(NuevaFacturacionBase)
+        self.label.setObjectName("label")
+        self.gridlayout.addWidget(self.label,0,0,1,1)
+
+        self.mui_nomempresa = QtGui.QLineEdit(NuevaFacturacionBase)
+        self.mui_nomempresa.setObjectName("mui_nomempresa")
+        self.gridlayout.addWidget(self.mui_nomempresa,0,1,1,1)
+        self.label_2.setBuddy(self.mui_nomdb)
+        self.label.setBuddy(self.mui_nomempresa)
+
         self.retranslateUi(NuevaFacturacionBase)
-        QtCore.QObject.connect(self.buttonBox,QtCore.SIGNAL("accepted()"),NuevaFacturacionBase.accept)
-        QtCore.QObject.connect(self.buttonBox,QtCore.SIGNAL("rejected()"),NuevaFacturacionBase.reject)
+        QtCore.QObject.connect(self.mui_botonera,QtCore.SIGNAL("accepted()"),NuevaFacturacionBase.accept)
+        QtCore.QObject.connect(self.mui_botonera,QtCore.SIGNAL("rejected()"),NuevaFacturacionBase.reject)
         QtCore.QMetaObject.connectSlotsByName(NuevaFacturacionBase)
 
     def retranslateUi(self, NuevaFacturacionBase):
         NuevaFacturacionBase.setWindowTitle(QtGui.QApplication.translate("NuevaFacturacionBase", "Nueva Facturacion", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("NuevaFacturacionBase", "Nombre Empresa", None, QtGui.QApplication.UnicodeUTF8))
-        self.label_2.setText(QtGui.QApplication.translate("NuevaFacturacionBase", "Nombre Base de Datos", None, QtGui.QApplication.UnicodeUTF8))
         self.groupBox.setTitle(QtGui.QApplication.translate("NuevaFacturacionBase", "Plugins", None, QtGui.QApplication.UnicodeUTF8))
+        self.mui_contratos.setText(QtGui.QApplication.translate("NuevaFacturacionBase", "Contratos de Servicios", None, QtGui.QApplication.UnicodeUTF8))
+        self.label_2.setText(QtGui.QApplication.translate("NuevaFacturacionBase", "Nombre Base de Datos", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("NuevaFacturacionBase", "Nombre Empresa", None, QtGui.QApplication.UnicodeUTF8))
 
