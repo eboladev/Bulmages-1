c4261cbe01f559bc06d915ab09a69532f6ee1330
Tomeu Borras <tborras@gmail.com>
2008-04-19 19:53:08 +0000

    Agrego el pluginresumcta que hace un resumen de la cuenta seleccionada en bulmacont. Concretamente indica su debe, haber, saldo y algunos datos de la entidad de la cuenta.

---
+++ bulmages/bulmacont/plugins/pluginresumcta/pluginresumcta.h
@@ -0,0 +1,37 @@
+/***************************************************************************
+ *   Copyright (C) 2005 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifdef Q_WS_WIN
+# define MY_EXPORT __declspec(dllexport)
+#else
+# define MY_EXPORT
+#endif
+
+#include "bulmacont.h"
+#include "postgresiface2.h"
+#include "empresa.h"
+#include "busquedacuenta.h"
+#include "subform2bc.h"
+
+extern "C" MY_EXPORT void entryPoint ( Bulmacont * );
+extern "C" MY_EXPORT int BusquedaCuenta_setidcuenta_post ( BusquedaCuenta * );
+extern "C" MY_EXPORT int BusquedaCuenta_s_lostFocus_post ( BusquedaCuenta * );
+extern "C" MY_EXPORT int SubForm2Bc_on_mui_list_cellChanged_post ( SubForm2Bc * );
+extern "C" MY_EXPORT int Bulmacont_closeEvent ( Bulmacont * );
