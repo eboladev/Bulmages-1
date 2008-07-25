8507631833cb79440bacc03861ea872a28166035
Leopold Palomo Avellaneda <leo@alaxarxa.net>
2008-07-25 18:25:03 +0000

    - Añado de nuevo los plugins balance-old y balance desde la Rev 2559. Hay problemas con los balances.

---
+++ bulmages/bulmacont/plugins/pluginbalance/balanceprintview.h
@@ -0,0 +1,60 @@
+/***************************************************************************
+ *   Copyright (C) 2003 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *   Copyright (C) 2003 by Antoni Mirabete i Teres                         *
+ *   amirabet@biada.org                                                    *
+ *   http://www.iglues.org                                                 *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#ifndef BALANCEPRINTVIEW_H
+#define BALANCEPRINTVIEW_H
+
+#include <errno.h>
+
+#include "ui_balanceprintbase.h"
+#include "postgresiface2.h"
+#include "empresa.h"
+#include "blwidget.h"
+
+/// Formulario introducción de datos para la impresión del balance de sumas y saldos.
+/** \todo Hay que cambiar el array de ccostes por un cursor.
+    Esta clase permite introducir los datos necesarios para la generaci&oacute;n del balance
+    (Fechas y rangos) y que se encarga de generar el balance en los formatos de
+    impresi&oacute;n indicados.
+*/
+class BalancePrintView : public QDialog, public Ui_BalancePrintBase, public PEmpresaBase
+{
+    Q_OBJECT
+
+
+public:
+    BalancePrintView ( Empresa *emp );
+    ~BalancePrintView();
+    void inicializa1 ( QString, QString, QString, QString, bool );
+
+private:
+    void presentar ( const char * tipus );
+
+private slots:
+    virtual void on_mui_imprimir_clicked();
+    virtual void on_mui_canales_clicked();
+    virtual void on_mui_ccostes_clicked();
+};
+
+#endif
+
