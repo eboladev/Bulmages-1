92b0f3cdf67c566bb38b84a30a6e221b9ad3bbde
Tomeu Borras <tborras@gmail.com>
2008-09-08 22:29:49 +0000

    Agrego el plugintccompras que permite introducir albaranes de compra a traves del TPV. NOTA: aun no esta terminado.

---
+++ bulmages/bulmatpv/plugins/plugintccompras/compra.h
@@ -0,0 +1,56 @@
+#ifndef DEVOLUCION_H
+#define DEVOLUCION_H
+
+#include "blwidget.h"
+#include <QTableWidget>
+#include "ui_comprabase.h"
+#include "empresatpv.h"
+#include "ticket.h"
+
+class MyDevButton1 : public QPushButton
+{
+    Q_OBJECT
+public:
+    EmpresaTPV *m_emp;
+    MyDevButton1 ( const QString & text, QWidget * parent = 0, EmpresaTPV *emp = NULL );
+    ~MyDevButton1();
+
+public slots:
+    virtual void on_click ( );
+};
+
+class Compra : public BLWidget, public Ui_CompraBase
+{
+    Q_OBJECT
+public:
+    int m_value;
+    Ticket *m_ticket;
+public:
+    Compra ( EmpresaTPV *emp, QWidget *parent );
+    virtual ~Compra();
+//    virtual void pintar();
+/*
+    void anadir ( int row );
+    void quitar ( int row );
+*/
+public slots:
+/*
+    virtual void on_mui_refLineEdit_returnPressed();
+    virtual void on_mui_todoButton_clicked();
+    virtual void on_mui_anadirButton_clicked();
+    virtual void on_mui_sacarButton_clicked();
+    virtual void on_mui_cancelarButton_clicked();
+
+    virtual void on_mui_valeButton_clicked();
+    virtual void on_mui_efectivoButton_clicked();
+    virtual void refreshDevolver();
+
+/// La parte de las teclas
+public slots:
+    virtual void ticketTableDoubleclick ( int row, int column );
+    virtual void devolverTableDoubleclick ( int row, int column );
+*/
+};
+
+#endif
+
