aea56f69bf2e24b1916281d59349676dd38b72f0
Fco. Javier M.C <fcojavmc@todo-redes.com>
2008-09-10 22:25:36 +0000

    Plugin Tarifas ahora funciona a falta de incorporar alguna funcionalidad. Corrijo el archivo 'installbulmages/bulmages.conf.in' alguien cambio erroneamente el puerto de Postgresql a 5433 en vez de 5432. El efecto es que no puede conectar con la base de datos y sale la ventana de nombre de usuario y contrasenya incorrecto. Lo digo por si a alguien le ha pasado que revise este parametro. Corrijo el tamanyo de algunos iconos dentro de las ventanas, supongo que es un bug al pasar a SVG.

--- bulmages/bulmalib/plugins/pluginimpers/pluginimpers.cpp
+++ bulmages/bulmalib/plugins/pluginimpers/pluginimpers.cpp
@@ -19,10 +19,10 @@
  *   Free Software Foundation, Inc.,                                       *
  *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
  ***************************************************************************/
+
 #include "pluginimpers.h"
 #include "funcaux.h"
 #include "eqtoolbutton.h"
-
 
 ///
 /**
