f69c2c9dc55f80210b14d732fc8a6015daeeaf57
Xavi Drudis Ferran <xdrudis@tinet.cat>
2008-09-24 13:41:37 +0000

    No posaa bé la contrasenya als usuaris que contenien majúscules.Intento escapar tots els caràcters possibles, però encara hi ha errors, per exemple per entrar a bulmafact amb una contrasenya que contigui apòstrof. I n sé perquè es restringeix el nom d'usuari i contrasenya a ascii

--- bulmages/bulmasetup/nuevousuario.py
+++ bulmages/bulmasetup/nuevousuario.py
@@ -17,13 +17,13 @@ class NuevoUsuario(QtGui.QDialog, Ui_NuevoUsuario):
 	
     def on_mui_botonera_accepted(self):
 	# Creamos el usuario
-	self.command = 'su postgres -c "createuser -s -d -r  ' + self.mui_nombre.text() +'"'
+	self.command = 'su postgres -c "createuser -s -d -r  \'' + self.mui_nombre.text().replace('\\','\\\\').replace('\'','\\\'') +'\'"'
 	self.process.start(self.command)
 	self.process.waitForFinished(-1)
 
 	# Cambiamos el password del usuario
-	self.subcomand = 'ALTER USER ' +self.mui_nombre.text()+ ' WITH PASSWORD \'\"\'' +self.mui_password.text() +'\'\"\' ;'
-	self.command = 'su postgres -c \'psql template1 -c \"' +self.subcomand+ '\"\''
+	self.subcomand = 'ALTER USER "' +self.mui_nombre.text().replace('"','""')+ '" WITH PASSWORD E\'' +self.mui_password.text().replace('\\','\\\\').replace('\'','\\\'') +'\' ;'
+	self.command = 'su postgres -c \'' + ('psql template1 -c \"' +self.subcomand.replace('\\','\\\\').replace('`','\\`').replace('$','\\$').replace('\n','\\\n').replace('"','\\"')+ '\"' ).replace('\'','\'"\'"\'')+'\''
         os.system(self.command.toAscii().data())
 	
 	self.accept()
