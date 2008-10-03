d8cd8a0a6cdf5f29b57f6f01c717804154197475
Tomeu Borras <tborras@gmail.com>
2008-10-03 18:34:06 +0000

    Agrego un nuevo e interesante plugin. Este lanza los parsers de metatags para las plantillas RML sobre plantillas python que generan informes de openoffice mediante ooolib. De esta forma podemos programar informes de hoja de calculo sin necesida de crear un plugin para ellos. Junto con el plugin adjunto una plantilla .pys de ejemplo que muestra un poco la capacidad de este tipo de informes. Tengo ganas de ver donde meten los empaquetadores las plantillas pys ya que son al mismo tiempo una plantilla de documento y un programa en python. JUAAAAAA JUAAAAAAAAAAAAA JUUUUAAAAAAAAAAAAAAAAAAAAAAA

---
+++ bulmages/bulmalib/plugins/plugininformesods/README.es
@@ -0,0 +1,26 @@
+#######
+#Plugins
+#######
+#README.es
+#20080501
+
+Nombre: Informes Personalizados en hoja de calculo
+
+Biblioteca: libplugininformesods.so
+
+Descripción: Presenta un nuevo menu Informes que permite lanzar los informes personalizados
+de que se disponga. Dichos informes son scripts Python con ooolib.
+
+ParmBD: 
+
+ArchivoSQLpatch: 
+
+ArchivoSQLdeins: 
+
+Dependencias:
+
+Incompatibles:
+
+Orden: 70
+
+Comentarios: Util para realizar informes de forma rápida sin tener que modificar el codigo fuente ni hacer un plugin específico para cada uno de ellos.
---
+++ bulmages/bulmalib/plugins/plugininformesods/plugininformesods.cpp
@@ -0,0 +1,182 @@
+/***************************************************************************
+ *   Copyright (C) 2008 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <stdio.h>
+
+#include <QAction>
+#include <QMessageBox>
+#include <QStringList>
+#include <QWidget>
+#include <QIcon>
+#include <QApplication>
+#include <QObject>
+#include <QProcess>
+#include <QTextStream>
+
+#include "plugininformesods.h"
+#include "funcaux.h"
+#include "bulmafact.h"
+#include "bulmacont.h"
+#include "myplugininformesods.h"
+#include "qworkspace2.h"
+#include "ficha.h"
+
+QMainWindow *g_bges = NULL;
+EmpresaBase *g_emp = NULL;
+
+
+///
+/**
+**/
+MyPluginInformesODS::MyPluginInformesODS()
+{
+    _depura ( "MyPluginInformesODS::MyPluginInformesODS", 0 );
+    _depura ( "END MyPluginInformesODS::MyPluginInformesODS", 0 );
+}
+
+///
+/**
+**/
+MyPluginInformesODS::~MyPluginInformesODS()
+{
+    _depura ( "MyPluginInformesODS::~MyPluginInformesODS", 0 );
+    _depura ( "END MyPluginInformesODS::~MyPluginInformesODS", 0 );
+}
+
+
+
+
+///
+/**
+**/
+void MyPluginInformesODS::elslot1( )
+{
+    _depura ( "MyPluginInformesODS::elslot", 0 );
+
+    Ficha *ficha = new Ficha ( g_emp, 0 );
+    if (!ficha->generaRML ( sender()->objectName() )) return;
+    QString archivod = sender()->objectName();
+    QString archivo =  sender()->objectName().left ( sender()->objectName().size() - 4 );
+
+    QString cadena = "rm " + confpr->valor ( CONF_DIR_USER ) + archivo + ".ods";
+    system ( cadena.toAscii() );
+
+    cadena = " cd " + confpr->valor ( CONF_DIR_USER ) + "; python " + archivod;
+    system ( cadena.toAscii() );
+    cadena = "oocalc " + confpr->valor ( CONF_DIR_USER ) + archivo + ".ods &";
+    system ( cadena.toAscii() );
+
+
+	delete ficha;
+    _depura ( "END MyPluginInformesODS::elslot", 0 );
+}
+
+void entryPoint ( QMainWindow *bges )
+{
+    _depura ( "Entrada del plugin Docket", 0 );
+
+    g_bges = bges;
+
+    MyPluginInformesODS *mcont = new MyPluginInformesODS;
+
+    QMenu *pPluginMenu = NULL;
+    /// Miramos si existe un menu Herramientas
+    pPluginMenu = bges->menuBar() ->findChild<QMenu *> ( "menuInformes" );
+    /// En BulmaCont el menu de Herramientas tiene otro nombre.
+    if ( !pPluginMenu ) {
+        pPluginMenu = bges->menuBar() ->findChild<QMenu *> ( "menuInformes" );
+    } // end if
+
+    /// Creamos el men&uacute;.
+    if ( !pPluginMenu ) {
+        pPluginMenu = new QMenu ( "&Informes ODS", bges->menuBar() );
+        pPluginMenu->setObjectName ( QString::fromUtf8 ( "InformesODS" ) );
+    } // end if
+
+    /*
+        pPluginMenu->addSeparator();
+        pPluginMenu->addAction ( accion );
+        pPluginMenu->addAction ( accion1 );
+    */
+
+//    QMenu *ajust = menu->addMenu ( tr ( "Informes Personales" ) );
+
+    /// Buscamos ficheros que tengan el nombre de la tabla
+    QDir dir ( confpr->valor ( CONF_DIR_OPENREPORTS ) );
+    dir.setFilter ( QDir::Files | QDir::NoSymLinks );
+    dir.setSorting ( QDir::Size | QDir::Reversed );
+    /// Hacemos un filtrado de busqueda
+    QStringList filters;
+    filters << "inf_*.pys";
+    dir.setNameFilters ( filters );
+
+
+    QFileInfoList list = dir.entryInfoList();
+    for ( int i = 0; i < list.size(); ++i ) {
+        QFileInfo fileInfo = list.at ( i );
+
+        QFile file;
+        file.setFileName ( confpr->valor ( CONF_DIR_OPENREPORTS ) + fileInfo.fileName() );
+        file.open ( QIODevice::ReadOnly );
+        QTextStream stream ( &file );
+        QString buff = stream.readAll();
+        file.close();
+
+        /// Buscamos el titulo
+        QString titulo = fileInfo.fileName();
+        QRegExp rx1 ( "title\\s*=\\s*\"(.*)\"" );
+        rx1.setMinimal ( TRUE );
+        if ( rx1.indexIn ( buff, 0 )  != -1 ) {
+            titulo = rx1.cap ( 1 );
+        } // end while
+
+
+        /// Creamos el men&uacute;.
+        QAction *accion = new QAction ( titulo, 0 );
+        accion->setObjectName ( fileInfo.fileName() );
+        accion->setStatusTip ( "Informe" );
+        accion->setWhatsThis ( "Informe" );
+        mcont->connect ( accion, SIGNAL ( activated() ), mcont, SLOT ( elslot1() ) );
+        pPluginMenu->addAction ( accion );
+    } // end for
+
+
+
+    /// A&ntilde;adimos la nueva opci&oacute;n al men&uacute; principal del programa.
+    bges->menuBar() ->insertMenu ( 0, pPluginMenu );
+
+    _depura ( "Iniciado correctamente el plugin dock", 10 );
+}
+
+
+
+
+int Company_createMainWindows_Post ( Company *cmp )
+{
+    g_emp = cmp;
+    return 0;
+}
+
+int Empresa_createMainWindows_Post ( Empresa *cmp )
+{
+    g_emp = cmp;
+    return 0;
+}
+
