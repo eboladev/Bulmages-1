e511ca2efe198a1adebaaa7f2488b9b33b103dfe
Fco. Javier M.C <fcojavmc@todo-redes.com>
2009-04-05 22:50:09 +0000

    Pongo g_confpr en estos archivos.

--- bulmages/bulmafact/plugins/pluginbf_alias/pluginbf_alias.cpp
+++ bulmages/bulmafact/plugins/pluginbf_alias/pluginbf_alias.cpp
@@ -45,7 +45,7 @@ int entryPoint ( Bulmafact *bges )
 
     /// Inicializa el sistema de traducciones 'gettext'.
     setlocale(LC_ALL, "");
-    bindtextdomain ("pluginbf_alias", confpr->valor(CONF_DIR_TRADUCCION).toAscii().constData());
+    bindtextdomain ("pluginbf_alias", g_confpr->valor(CONF_DIR_TRADUCCION).toAscii().constData());
 
     g_ultimRefrescAlias.start();
     return 0;
