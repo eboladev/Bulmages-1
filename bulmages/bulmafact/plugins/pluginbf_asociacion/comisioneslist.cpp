f20414550d6c9ce913dd95aa2dba64ba378b78ab
Tomeu Borras <tborras@gmail.com>
2009-04-25 12:25:04 +0000

    Agrego archivos que me deje por subir (de comisiones).

---
+++ bulmages/bulmafact/plugins/pluginbf_asociacion/comisioneslist.cpp
@@ -0,0 +1,276 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QMessageBox>
+#include <QCheckBox>
+#include <QFile>
+#include <QCheckBox>
+#include <QTextStream>
+#include <QMenu>
+
+#include "blconfiguration.h"
+#include "comisioneslist.h"
+#include "bfcompany.h"
+#include "comisionview.h"
+#include "blfunctions.h"
+
+
+/** Inicializa todos los componentes.
+    Mete la ventana en el workSpace.
+    Este constructor no es completo, debe inicializarse con setcompany para que la clase pueda operar.
+*/
+ComisionesList::ComisionesList ( QWidget *parent, Qt::WFlags flag, edmode editmodo )
+        : BlFormList ( NULL, parent, flag, editmodo )
+{
+    _depura ( "ComisionesList::ComisionesList", 0 );
+    setAttribute ( Qt::WA_DeleteOnClose );
+    setupUi ( this );
+    /// Disparamos los plugins.
+    int res = g_plugins->lanza ( "ComisionesList_ComisionesList", this );
+    if ( res != 0 )
+        return;
+    mdb_idjdirectiva = "";
+    setSubForm ( mui_list );
+    hideBusqueda();
+
+    _depura ( "END ComisionesList::ComisionesList", 0 );
+}
+
+
+/** Inicializa todos los componentes.
+    Hace una presentacion inicial.
+    Mete la ventana en el workSpace.
+*/
+ComisionesList::ComisionesList ( BfCompany *comp, QWidget *parent, Qt::WFlags flag, edmode editmodo )
+        : BlFormList ( comp, parent, flag, editmodo )
+{
+    _depura ( "ComisionesList::ComisionesList", 0 );
+    setAttribute ( Qt::WA_DeleteOnClose );
+    setupUi ( this );
+    /// Disparamos los plugins.
+    int res = g_plugins->lanza ( "ComisionesList_ComisionesList", this );
+    if ( res != 0 )
+        return;
+
+    mui_list->setMainCompany ( comp );
+
+
+    setSubForm ( mui_list );
+    presentar();
+    mdb_idjdirectiva = "";
+    if ( modoEdicion() )
+        mainCompany() ->meteWindow ( windowTitle(), this );
+    hideBusqueda();
+    /// Hacemos el tratamiento de los permisos que desabilita botones en caso de no haber suficientes permisos.
+    trataPermisos ( "cobro" );
+    _depura ( "END ComisionesList::ComisionesList", 0 );
+}
+
+
+/** No requiere acciones especiales en el destructor.
+*/
+ComisionesList::~ComisionesList()
+{
+    _depura ( "ComisionesList::~ComisionesList", 0 );
+    _depura ( "END ComisionesList::~ComisionesList", 0 );
+}
+
+
+/** Hace la carag del listado.
+    PAra ello genera los SELECTS con ayuda de generaFiltro y los pasa al SubFormulario para que los presente.
+    Tambien hace un select de calculo de totales y lo presenta en el textEdit correspondiente.
+*/
+void ComisionesList::presentar()
+{
+    _depura ( "ComisionesList::presentar", 0 );
+    if ( mainCompany() != NULL ) {
+        mui_list->cargar ( "SELECT * FROM jdirectiva  WHERE 1 = 1 " + generaFiltro() );
+    } // end if
+    _depura ( "END ComisionesList::presentar", 0 );
+}
+
+
+/** Metodo auxiliar que genera la clausula WHERE del listado con las opciones de filtrado especificadas.
+*/
+QString ComisionesList::generaFiltro()
+{
+    _depura ( "ComisionesList::generaFiltro", 0 );
+    QString filtro = "";
+
+    _depura ( "END ComisionesList::generaFiltro", 0 );
+    return ( filtro );
+}
+
+
+
+/** SLOT que responde a la pulsacion del boton de crear en el listado.
+    Instancia la clase ComisionView, y la presenta.
+*/
+void ComisionesList::crear()
+{
+    _depura ( "ComisionesList::crear", 0 );
+    ComisionView *bud = new ComisionView ( ( BfCompany * ) mainCompany(), 0 );
+    mainCompany() ->m_pWorkspace->addWindow ( bud );
+    bud->show();
+    bud->pintar();
+    _depura ( "ComisionesList::crear", 0 );
+}
+
+
+/** La impresion de listados esta completamente delegada a la clase SubForm3
+*/
+void ComisionesList::imprimir()
+{
+    _depura ( "ComisionesList::imprimir", 0 );
+    mui_list->imprimirPDF ( _ ( "Actividades" ) );
+    _depura ( "END ComisionesList::imprimir", 0 );
+}
+
+
+/** SLOT que responde a la pulsacion del boton borrar.
+    Comprueba que exista un elemento seleccionado.
+    Instancia la clase ComisionView, la inicializa y le lanza el metodo borrar.
+    Esta es la forma correcta de implementar un borrado a partir de un listado
+    ya que de esta forma si existen plugins que alteren el borrado tambien seran invocados.
+*/
+void ComisionesList::borrar()
+{
+    _depura ( "ComisionesList::borrar", 0 );
+    int a = mui_list->currentRow();
+    if ( a < 0 ) {
+        mensajeInfo ( _ ( "Debe seleccionar una linea" ) );
+        return;
+    } // end if
+    try {
+        mdb_idjdirectiva = mui_list->dbValue ( "idjdirectiva" );
+        if ( modoEdicion() ) {
+            ComisionView * cv = new ComisionView ( ( BfCompany * ) mainCompany(), 0 );
+            if ( cv->cargar ( mdb_idjdirectiva ) )
+                throw - 1;
+            cv->on_mui_borrar_clicked();
+            cv->close();
+        } // end if
+        presentar();
+    } catch ( ... ) {
+        mensajeInfo ( _ ( "Error al borrar el cobro a cliente" ) );
+    } // end try
+    _depura ( "END:ComisionesList::borrar", 0 );
+}
+
+
+/** SLOT que responde al doble click en el subformulario.
+    Dependiendo del modo (Edicion o Seleccion) hace unas operaciones u otras.
+*/
+/// \TODO: Deberia crearse el metodo editar y este llamar a ese.
+void ComisionesList::editar ( int )
+{
+    _depura ( "ComisionesList::on_mui_list_cellDoubleClicked", 0 );
+    try {
+        mdb_idjdirectiva = mui_list->dbValue ( "idjdirectiva" );
+        if ( modoEdicion() ) {
+            ComisionView * bud = new ComisionView ( ( BfCompany * ) mainCompany(), 0 );
+            if ( bud->cargar ( mdb_idjdirectiva ) ) {
+                delete bud;
+                return;
+            } // end if
+            mainCompany() ->m_pWorkspace->addWindow ( bud );
+            bud->show();
+        } else {
+            emit ( selected ( mdb_idjdirectiva ) );
+        } // end if
+    } catch ( ... ) {
+        mensajeInfo ( _ ( "Debe seleccionar una fila primero" ) );
+    } // end try
+    _depura ( "END ComisionesList::on_mui_list_cellDoubleClicked", 0 );
+
+}
+
+/** SLOT que responde a la peticion de menu contextual en el subformulario.
+*/
+/// \TODO: Revisar si este metodo es util.
+void ComisionesList::submenu ( const QPoint & )
+{
+    _depura ( "ComisionesList::on_mui_list_customContextMenuRequested", 0 );
+    int a = mui_list->currentRow();
+    if ( a < 0 )
+        return;
+    QMenu *popup = new QMenu ( this );
+    QAction *edit = popup->addAction ( _ ( "Editar Junta" ) );
+    QAction *del = popup->addAction ( _ ( "Borrar Junta" ) );
+    QAction *opcion = popup->exec ( QCursor::pos() );
+    if ( opcion == del )
+        on_mui_borrar_clicked();
+    if ( opcion == edit )
+        on_mui_editar_clicked();
+    delete popup;
+    _depura ( "ComisionesList::on_mui_list_customContextMenuRequested", 0 );
+}
+
+/** Inicializa la clase con el puntero a la company que se esta utilizando
+**/
+void ComisionesList::setMainCompany ( BfCompany *comp )
+{
+    BlMainCompanyPointer::setMainCompany ( comp );
+//    mui_list->setMainCompany ( comp );
+}
+
+/** Devuelve el identificador del cobro seleccionado
+**/
+QString ComisionesList::idjdirectiva()
+{
+    _depura ( "ComisionesList::idjdirectiva", 0 );
+    _depura ( "END ComisionesList::idjdirectiva", 0 );
+    return mdb_idjdirectiva;
+}
+
+
+
+/// =============================================================================
+///                    SUBFORMULARIO
+/// =============================================================================
+/** Prepara el subformulario para trabajar con la tabla cobro.
+    Establece que no se puedan insertar elementos ya que es un listado.
+*/
+ComisionesListSubForm::ComisionesListSubForm ( QWidget *parent ) : BfSubForm ( parent )
+{
+    _depura ( "ComisionesListSubForm::ComisionesListSubForm", 0 );
+    /// Disparamos los plugins.
+    int res = g_plugins->lanza ( "ComisionesListSubForm_ComisionesListSubForm", this );
+    if ( res != 0 )
+        return;
+    setDbTableName ( "jdirectiva" );
+    setDbFieldId ( "idjdirectiva" );
+    addSubFormHeader ( "idjdirectiva", BlDbField::DbInt, BlDbField::DbNotNull | BlDbField::DbPrimaryKey, BlSubFormHeader::DbHideView | BlSubFormHeader::DbNoWrite, _ ( "ID Junta Directiva" ) );
+    addSubFormHeader ( "fechainjdirectiva", BlDbField::DbVarChar, BlDbField::DbNoSave, BlSubFormHeader::DbNone | BlSubFormHeader::DbNoWrite, _ ( "Constitucion" ) );
+    addSubFormHeader ( "fechafinjdirectiva", BlDbField::DbVarChar, BlDbField::DbNoSave, BlSubFormHeader::DbNone | BlSubFormHeader::DbNoWrite, _ ( "Constitucion" ) );
+    
+    setInsert ( FALSE );
+    setDelete ( FALSE );
+    setSortingEnabled ( TRUE );
+    _depura ( "END ComisionesListSubForm::ComisionesListSubForm", 0 );
+}
+
+
+ComisionesListSubForm::~ComisionesListSubForm()
+{
+    _depura ( "ComisionesListSubForm::~ComisionesListSubForm", 0 );
+    _depura ( "END ComisionesListSubForm::~ComisionesListSubForm", 0 );
+}
+
---
+++ bulmages/bulmafact/plugins/pluginbf_asociacion/comisionview.cpp
@@ -0,0 +1,145 @@
+/***************************************************************************
+ *   Copyright (C) 2004 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <fstream>
+
+#include <QMessageBox>
+#include <QCloseEvent>
+#include <QFile>
+#include <QTextStream>
+
+#include "comisionview.h"
+#include "bfcompany.h"
+#include "blconfiguration.h"
+#include "bldatesearch.h"
+#include "blfunctions.h"
+
+
+/** inicializa todos los componentes de la clase.
+    Resetea el sistema de control de cambios para que considere que no hay cambios por parte del usuario.
+    Mete la ventana en el workSpace.
+*/
+ComisionView::ComisionView ( BfCompany *comp, QWidget *parent )
+        : BfForm ( comp, parent )
+{
+    _depura ( "ComisionView::ComisionView", 0 );
+    setAttribute ( Qt::WA_DeleteOnClose );
+    try {
+        setupUi ( this );
+        centrarEnPantalla ( this );
+
+        setTitleName ( _ ( "Comision" ) );
+        setDbTableName ( "jdirectiva" );
+        setDbFieldId ( "idjdirectiva" );
+        addDbField ( "idjdirectiva", BlDbField::DbInt, BlDbField::DbPrimaryKey, _ ( "ID jdirectiva" ) );
+        addDbField ( "fechainjdirectiva", BlDbField::DbDate, BlDbField::DbNotNull, _ ( "Fecha Constitucion" ) );
+        addDbField ( "fechafinjdirectiva", BlDbField::DbDate, BlDbField::DbNothing, _ ( "Fecha Cese" ) );
+
+        mui_list->setMainCompany( comp );
+        mui_list->setDbTableName ( "miembrojdirectiva" );
+        mui_list->setDbFieldId ( "idmiembrojdirectiva" );
+        mui_list->addSubFormHeader ( "idmiembrojdirectiva", BlDbField::DbInt, BlDbField::DbPrimaryKey, BlSubFormHeader::DbHideView | BlSubFormHeader::DbNoWrite, _ ( "ID Miembro" ) );
+        mui_list->addSubFormHeader ( "idjdirectiva", BlDbField::DbInt, BlDbField::DbNothing, BlSubFormHeader::DbHideView | BlSubFormHeader::DbNoWrite, _ ( "ID Junta Directiva" ) );
+        mui_list->addSubFormHeader ( "fechainmiembrojdirectiva", BlDbField::DbDate, BlDbField::DbNothing, BlSubFormHeader::DbNone , _ ( "Fecha posesion" ) );
+        mui_list->addSubFormHeader ( "fechafinmiembrojdirectiva", BlDbField::DbDate, BlDbField::DbNothing, BlSubFormHeader::DbNone, _ ( "Fecha Cese" ) );
+        mui_list->addSubFormHeader ( "idcliente", BlDbField::DbInt, BlDbField::DbNotNull | BlDbField::DbRequired, BlSubFormHeader::DbHideView | BlSubFormHeader::DbNoWrite, _ ( "ID Socio" ) );
+        mui_list->addSubFormHeader ( "nomcliente", BlDbField::DbVarChar, BlDbField::DbNoSave, BlSubFormHeader::DbNone, _ ( "Socio" ) );
+        mui_list->addSubFormHeader ( "cargomiembrojdirectiva", BlDbField::DbVarChar, BlDbField::DbNothing, BlSubFormHeader::DbNone, _ ( "Cargo" ) );
+
+        mui_list->setInsert ( TRUE );
+        mui_list->setDelete ( TRUE );
+        mui_list->setSortingEnabled ( FALSE );
+        
+        mui_list->cargar("SELECT * from miembrojdirectiva NATURAL LEFT JOIN cliente WHERE 1 = 2");
+        
+        meteWindow ( windowTitle(), this, FALSE );
+        pintar();
+        dialogChanges_cargaInicial();
+    } catch ( ... ) {
+        mensajeInfo ( _ ( "Error al crear el jdirectiva" ), this );
+    } // end try
+    _depura ( "END ComisionView::ComisionView", 0 );
+}
+
+
+/** No precisa acciones adicionales en el destructor.
+*/
+ComisionView::~ComisionView()
+{
+    _depura ( "ComisionView::~ComisionView", 0 );
+    _depura ( "END ComisionView::~ComisionView", 0 );
+}
+
+
+QString ComisionView::nombrePlantilla ( void )
+{
+    return QString ( "jdirectiva" );
+}
+
+void ComisionView::imprimir()
+{
+    _depura ( "ComisionView::imprimir", 0 );
+    /// Comprobamos que se disponen de los datos minimos para imprimir el jdirectiva.
+    QString SQLQuery = "";
+
+    if ( dbValue ( "idjdirectiva" ).isEmpty() ) {
+        /// El documento no se ha guardado y no se dispone en la base de datos de estos datos.
+        mensajeInfo ( _ ( "Tiene que guardar el documento antes de poder imprimirlo." ), this );
+        return;
+    }
+    /// Disparamos los plugins
+    int res = g_plugins->lanza ( "ComisionView_on_mui_imprimir_clicked", this );
+    if ( res != 0 ) {
+        return;
+    } // end if
+    BfForm::imprimir();
+
+    _depura ( "END ComisionView::imprimir", 0 );
+}
+
+
+int ComisionView::guardarPost()
+{
+    _depura ( " ComisionView::guardarPost", 0 );
+    mui_list->setColumnValue("idjdirectiva", dbValue("idjdirectiva") );
+    mui_list->guardar();
+    _depura ( "END ComisionView::guardarPost", 0 );
+}
+
+
+int ComisionView::borrarPre()
+{
+
+    return 0;
+}
+
+
+
+int ComisionView::cargarPost ( QString id )
+{
+    _depura ( " ComisionView::cargarPost", 0 );
+    mui_list->cargar("SELECT * FROM miembrojdirectiva NATURAL LEFT JOIN cliente WHERE idjdirectiva = " + id);
+    _depura ( "END ComisionView::cargarPost", 0 );
+    return 0;
+}
+
+
+
+
