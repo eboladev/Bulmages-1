428dc893882e1519368a99c868194ca7fbe02aec
Tomeu Borras <tborras@gmail.com>
2009-05-05 07:29:55 +0000

    Cambio del diseño de pantalla de bulmasetup para que coja el  nuevo icono. Tambien cambio el icono del about de bulmafact.

--- bulmages/bulmasetup/bulmasetupbase.py
+++ bulmages/bulmasetup/bulmasetupbase.py
@@ -2,7 +2,7 @@
 
 # Form implementation generated from reading ui file 'bulmasetupbase.ui'
 #
-# Created: Sun May  3 18:58:45 2009
+# Created: Tue May  5 09:12:18 2009
 #      by: PyQt4 UI code generator 4.4.4
 #
 # WARNING! All changes made in this file will be lost!
