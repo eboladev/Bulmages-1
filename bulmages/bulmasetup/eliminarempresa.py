a5bc384fc09c09363358c92e22c593cab1007f6c
Tony Alvarez <t.alvarezpascual@gmail.com>
2009-05-18 15:23:16 +0000

    ya está terminada la función de eliminar empresas de bulmasetup, añado el icono y el boton a bulmasetup.

--- bulmages/bulmasetup/eliminarempresa.py
+++ bulmages/bulmasetup/eliminarempresa.py
@@ -16,6 +16,7 @@ class EliminarEmpresa(Ui_EliminarEmpresa, Empresa):
         self.setupUi(self)
         self.show()
         self.buscarEmpresas()
+        self.lista_empresas.resizeColumnsToContents()
 
     def buscarEmpresas(self):
     
@@ -23,19 +24,57 @@ class EliminarEmpresa(Ui_EliminarEmpresa, Empresa):
         self.databases = self.execute("SELECT datname FROM pg_database ORDER BY datname")
         self.desconectar()
         
+        self.lista_empresas.setRowCount(0)
+        self.i = 0
         for row in self.databases:
             if (str(row[0]) != 'template0' and str(row[0]) != 'template1' ):
+                self.lista_empresas.hideRow(self.i)
                 self.conectar(row[0])
-                existconf = self.executeone('SELECT * FROM pg_tables WHERE tablename =\'configuracion\'')
+                existconf = self.executeone('SELECT * FROM pg_tables where tablename =\'configuracion\'')
                 if (existconf != None):
                     tipo = self.executeone('SELECT valor FROM configuracion where nombre =\'Tipo\'')
                     nombre = self.executeone('SELECT valor FROM configuracion where nombre =\'NombreEmpresa\'')
-                    if (str(tipo[0]) == 'BulmaCont'):
-                        self.listWidgetCont.addItem(QString(nombre[0]))
-                    if (str(tipo[0]) == 'BulmaFact'):
-                        self.listWidgetFact.addItem(QString(nombre[0]))
+                    self.lista_empresas.setRowCount(self.lista_empresas.rowCount() + 1)
+                    self.lista_empresas.setItem(self.i, 0 , QTableWidgetItem(str(nombre[0])))
+                    self.lista_empresas.setItem(self.i, 1 , QTableWidgetItem(row[0]))
+                    self.lista_empresas.setItem(self.i, 2 , QTableWidgetItem(str(tipo[0])))
+                    self.lista_empresas.showRow(self.i)
+                    self.i = self.i + 1
                 self.desconectar()
-
+                
+    def on_lista_empresas_cellDoubleClicked(self, row, col):
+    
+        Yes = 'Si'
+        No = 'No'
+        respuesta = False        
+        message = QtGui.QMessageBox(self)
+        message.setText('Seguro que deseas eliminar esta empresa ?')
+        message.setWindowTitle('Confirmacion')
+        message.setIcon(QtGui.QMessageBox.Question)
+        message.addButton(Yes, QtGui.QMessageBox.AcceptRole)
+        message.addButton(No, QtGui.QMessageBox.RejectRole)
+        message.exec_()        
+        respuesta = message.clickedButton().text()
+        
+        if respuesta == Yes:
+            self.proceso = QtCore.QProcess()
+            command = 'su postgres -c "dropdb ' + str(self.lista_empresas.item(row,1).text()) + '"'
+            print command
+            self.proceso.start(command)
+            self.proceso.waitForFinished(-1)
+                    
+            if (self.lista_empresas.item(row,2).text() == QString('BulmaFact')):
+                if os.path.exists('/etc/bulmages/bulmafact_' + str(self.lista_empresas.item(row,1).text()) + '.conf'):
+                    os.remove('/etc/bulmages/bulmafact_' + str(self.lista_empresas.item(row,1).text()) + '.conf')
+                if os.path.exists('/etc/bulmages/bulmatpv_' + str(self.lista_empresas.item(row,1).text()) + '.conf'):
+                    os.remove('/etc/bulmages/bulmatpv_' + str(self.lista_empresas.item(row,1).text()) + '.conf')
+            if (self.lista_empresas.item(row,2).text() == QString('BulmaCont')):
+                if os.path.exists('/etc/bulmages/bulmacont_' + str(self.lista_empresas.item(row,1).text()) + '.conf'):
+                    os.remove('/etc/bulmages/bulmacont_' + str(self.lista_empresas.item(row,1).text()) + '.conf')
+                    
+            self.lista_empresas.clear()
+            self.buscarEmpresas()
+                    
 def main(args):
     app=QtGui.QApplication(args)
     win=EliminarEmpresa()
