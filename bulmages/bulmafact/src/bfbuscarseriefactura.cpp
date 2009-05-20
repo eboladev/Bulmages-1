019fd8379993afee25d47320ab46972ecef2b307
Tomeu Borras <tborras@gmail.com>
2009-05-20 10:53:26 +0000

    Arreglo el fallo de la serie de factura por defecto. El dato puede estar tanto en la tabla de configuraciones como en la configuracion con CONF_SERIE_FACTURA_DEFECTO. Al final el parametro de la tabla de configuracion tendera a desaparecer porque no permite tener configuraciones diferentes para usuarios diferentes mientras que el parametro en el archivo de configuracion si.

--- bulmages/bulmafact/src/bfbuscarseriefactura.cpp
+++ bulmages/bulmafact/src/bfbuscarseriefactura.cpp
@@ -66,9 +66,19 @@ void BfBuscarSerieFactura::setId ( QString codigo )
 {
     _depura ( "BfBuscarSerieFactura::setcodigoserie_factura", 0 );
     /// Si lo que se pasa como serie es un valor malo cogemos la serie de factura por defecto.
+    /// Prevalece el valor de la configuracion en la base de datos sobre el valor en el archivo de configuracion.
+
     if ( codigo.isEmpty() || codigo == "0" ) {
         codigo = g_confpr->valor ( CONF_SERIE_FACTURA_DEFECTO );
     } // end if
+
+    BlDbRecordSet *cur = mainCompany() ->loadQuery ( "SELECT * FROM configuracion WHERE nombre ='SerieFacturaDefecto'" );
+    if ( !cur ) return;
+    if ( !cur->eof() ) {
+        codigo = cur->valor ( "valor" );
+    } // end if
+    delete cur;
+
 
     if ( m_comboRecordSet != NULL )
         delete m_comboRecordSet;
