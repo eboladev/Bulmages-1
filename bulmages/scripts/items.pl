da580a09e4a193ec5600a1879a07c5051fa544a2
Xavi Drudis Ferran <xdrudis@tinet.cat>
2009-08-16 16:34:22 +0000

    Aquest mini script no anava bé. Generava un tros d'xml sense escapar entitats de caracter ni res. Ara l'he liat una mica i genera un full XSLT que quan l'apliques (amb xsltproc, per exemple) a un ui canvia els items d'un combo. Ara li passes el nom del combo i els fitxers de text d'on treure les línies. Són coses que no es faran gaire sovint, sospito, només serveix per modificar els llistats d'etiquetes quan s'importen noves mides.

--- bulmages/scripts/items.pl
+++ bulmages/scripts/items.pl
@@ -1,9 +1,33 @@
 #!/usr/bin/perl -w
+use XML::EasyOBJ;
+my $i=0;
+
+if ($#ARGV<0) {
+   die "ús: items.pl nomCombo fitx1.txt ... fitxN.txt\nGenera un xslt que canvia els items del combo amb nom el primer argument per les línies dels fitxers de text passats.\n"
+}
+$nomCombo = shift @ARGV;
+#$doc =  new XML::EasyOBJ(-type => 'new', -param => 'widget');
+$doc =  new XML::EasyOBJ(-type => 'new', -param => 'xsl:stylesheet');
+$doc->setAttr('version','1.0','xmlns:xsl','http://www.w3.org/1999/XSL/Transform');
+# el getElement crea l'ement si no existeix
+$t=$doc->getElement('xsl:template',0);
+$t->setAttr('match','node()|@*');
+$t->getElement('xsl:copy')->getElement('xsl:apply-templates')->setAttr('select','node()|@*');
+
+
+$t=$doc->getElement('xsl:template',1);
+$t->setAttr('match','widget[@name=\''.$nomCombo.'\']');
+$c=$t->getElement('xsl:copy');
+$f=$c->getElement('xsl:for-each');
+$f->setAttr('select','*[name()!=\'item\']|@*');
+$f->getElement('xsl:copy')->getElement('xsl:apply-templates')->setAttr('select','node()|@*');
+
 while(<>) {
   chomp;
-  print "  <item>\n",
-        '    <property name="text">',"\n",
-        "     <string>$_</string>\n",
-        "    </property>\n",
-        "  </item>\n";
+  $c->item($i)->property()->setAttr('name','text');
+  $c->item($i)->property()->string->setString($_);
+  $i++;
 }
+
+print $doc->getDomObj->toString;
+
