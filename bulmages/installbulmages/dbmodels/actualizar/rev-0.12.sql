24761ef8031ee85662a11b0d6f2cdfeb5a09e8e0
Tomeu Borras <tborras@gmail.com>
2009-11-07 09:25:22 +0000

    Actualizo algunos triggers de bulmacont con versiones simplificadas utilizando TG_OP para hacer la distincion de casos

--- bulmages/installbulmages/dbmodels/actualizar/rev-0.12.sql
+++ bulmages/installbulmages/dbmodels/actualizar/rev-0.12.sql
@@ -81,6 +81,158 @@ DROP FUNCTION compruebarevision() CASCADE;
 -- ================================= A PARTIR DE AQUI EL PARCHE=========================
 -- =====================================================================================
 
+SELECT drop_if_exists_proc('aumenta_valor', '');
+SELECT drop_if_exists_proc('disminuye_valor', '');
+SELECT drop_if_exists_proc('disminuye_valor1', '');
+SELECT drop_if_exists_proc('trataacumulados', '');
+
+\echo -n ':: Funcion aumenta valor ... '
+
+CREATE FUNCTION trataacumulados() RETURNS "trigger"
+    AS '
+DECLARE
+    cta int4;
+    ccost int4;
+    ctar RECORD;
+    ccostr RECORD;
+BEGIN
+  -- Si se trata de una insercion hacemos los calculos con el campo NEW
+  IF TG_OP = ''INSERT'' THEN
+	  UPDATE cuenta SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idcuenta = NEW.idcuenta;
+	  UPDATE c_coste SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idc_coste = NEW.idc_coste;
+	  IF NEW.idcuenta IS NOT NULL THEN
+		  UPDATE acumulado_canal SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idcuenta = NEW.idcuenta AND idcanal = NEW.idcanal;
+	  END IF;
+	  cta := NEW.idcuenta;
+	  ccost := NEW.idc_coste;
+	  -- RAISE NOTICE '' Se ha lanzado la funcion aumenta_valor()'';
+	  SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccost;
+	  WHILE FOUND LOOP
+		  SELECT INTO ctar * FROM cuenta WHERE idcuenta = cta;
+		  WHILE FOUND LOOP
+		  -- RAISE NOTICE '' Cuenta % Centro Coste %'', ctar.idcuenta, ccostr.idc_coste;
+		  UPDATE acumulado_c_coste SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idc_coste = ccostr.idc_coste AND idcuenta = ctar.idcuenta;
+		  SELECT INTO ctar * FROM cuenta WHERE idcuenta = ctar.padre;
+		  END LOOP;
+		  SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccostr.padre;
+	  END LOOP;
+      RETURN NEW;
+  END IF;
+
+  -- Si se trata de un borrado se hacen los calculos con el campo OLD
+  IF TG_OP = ''DELETE'' THEN
+    UPDATE cuenta SET debe = debe - OLD.debe, haber = haber - OLD.haber WHERE idcuenta = OLD.idcuenta;
+    UPDATE c_coste SET debe = debe - OLD.debe, haber = haber - OLD.haber WHERE idc_coste = OLD.idc_coste;
+    IF OLD.idcuenta IS NOT NULL THEN
+	UPDATE acumulado_canal SET debe= debe - OLD.debe, haber =haber - OLD.haber WHERE idcuenta = OLD.idcuenta AND idcanal = OLD.idcanal;
+    END IF;
+    cta := OLD.idcuenta;
+    ccost := OLD.idc_coste;
+    -- RAISE NOTICE '' Se ha lanzado la funcion disminuye_valor()'';
+    SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccost;
+    WHILE FOUND LOOP
+	SELECT INTO ctar * FROM cuenta WHERE idcuenta = cta;
+	WHILE FOUND LOOP
+	    -- RAISE NOTICE '' Cuenta % Centro Coste %'', ctar.idcuenta, ccostr.idc_coste;
+	    UPDATE acumulado_c_coste SET debe = debe - OLD.debe, haber = haber -OLD.haber WHERE idc_coste = ccostr.idc_coste AND idcuenta = ctar.idcuenta;
+	    SELECT INTO ctar * FROM cuenta WHERE idcuenta = ctar.padre;
+	END LOOP;
+	SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccostr.padre;
+    END LOOP;
+    -- RAISE NOTICE '' disminuye_valor: Finaliza el algoritmo. '';
+    RETURN OLD;
+  END IF;
+
+  IF TG_OP = ''UPDATE'' THEN
+    IF NEW.debe <> OLD.debe OR NEW.haber <> OLD.haber THEN
+	-- RAISE NOTICE ''disminuye_valor: debe antiguo %, debe nuevo %'', OLD.debe, NEW.debe;
+	UPDATE cuenta SET debe = debe - OLD.debe, haber = haber - OLD.haber + NEW.haber WHERE idcuenta = OLD.idcuenta;
+	UPDATE c_coste SET debe = debe - OLD.debe, haber = haber - OLD.haber WHERE idc_coste = OLD.idc_coste;
+	IF OLD.idcuenta IS NOT NULL THEN
+		UPDATE acumulado_canal SET debe= debe - OLD.debe, haber =haber - OLD.haber WHERE idcuenta = OLD.idcuenta AND idcanal = OLD.idcanal;
+	END IF;
+	cta := OLD.idcuenta;
+	ccost := OLD.idc_coste;
+	-- RAISE NOTICE '' Se ha lanzado la funcion disminuye_valor()'';
+	SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccost;
+	WHILE FOUND LOOP
+		SELECT INTO ctar * FROM cuenta WHERE idcuenta = cta;
+		WHILE FOUND LOOP
+		-- RAISE NOTICE '' Cuenta % Centro Coste %'', ctar.idcuenta, ccostr.idc_coste;
+		UPDATE acumulado_c_coste SET debe = debe - OLD.debe, haber = haber -OLD.haber WHERE idc_coste = ccostr.idc_coste AND idcuenta = ctar.idcuenta;
+		SELECT INTO ctar * FROM cuenta WHERE idcuenta = ctar.padre;
+		END LOOP;
+		SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccostr.padre;
+	END LOOP;
+
+	UPDATE cuenta SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idcuenta = NEW.idcuenta;
+	UPDATE c_coste SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idc_coste = NEW.idc_coste;
+	IF NEW.idcuenta IS NOT NULL THEN
+		UPDATE acumulado_canal SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idcuenta = NEW.idcuenta AND idcanal = NEW.idcanal;
+	END IF;
+	cta := NEW.idcuenta;
+	ccost := NEW.idc_coste;
+	-- RAISE NOTICE '' Se ha lanzado la funcion aumenta_valor()'';
+	SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccost;
+	WHILE FOUND LOOP
+		SELECT INTO ctar * FROM cuenta WHERE idcuenta = cta;
+		WHILE FOUND LOOP
+		-- RAISE NOTICE '' Cuenta % Centro Coste %'', ctar.idcuenta, ccostr.idc_coste;
+		UPDATE acumulado_c_coste SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idc_coste = ccostr.idc_coste AND idcuenta = ctar.idcuenta;
+		SELECT INTO ctar * FROM cuenta WHERE idcuenta = ctar.padre;
+		END LOOP;
+		SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccostr.padre;
+	END LOOP;
+
+
+	-- RAISE NOTICE '' disminuye_valor: Finaliza el algoritmo. '';
+    END IF;
+    RETURN NEW;
+  END IF;
+
+END;
+'
+    LANGUAGE plpgsql;
+
+
+\echo -n ':: Disparador nuevo apunte aumente valor ... '
+CREATE TRIGGER trataacumuladost
+    AFTER INSERT OR UPDATE OR DELETE ON apunte
+    FOR EACH ROW
+    EXECUTE PROCEDURE trataacumulados();
+
+
+SELECT drop_if_exists_proc('cambiadoiva', '');
+SELECT drop_if_exists_proc('cambiadoivad', '');
+
+\echo -n ':: Funcion cambiado IVA ... '
+CREATE OR REPLACE FUNCTION cambiadoiva() RETURNS "trigger"
+    AS '
+DECLARE
+    mrecord RECORD;
+BEGIN
+  IF TG_OP = ''INSERT'' OR TG_OP =''UPDATE'' THEN
+    FOR mrecord IN SELECT SUM(baseiva) AS suma, SUM(ivaiva) AS sumaiva FROM iva WHERE iva.idregistroiva = NEW.idregistroiva LOOP
+    	UPDATE registroiva SET baseimp = mrecord.suma, iva = mrecord.sumaiva WHERE idregistroiva = NEW.idregistroiva;
+    END LOOP;
+    RETURN NEW;
+  END IF;
+
+  IF TG_OP = ''DELETE'' THEN
+    FOR mrecord IN SELECT SUM(baseiva) AS suma, SUM(ivaiva) AS sumaiva FROM iva WHERE iva.idregistroiva = OLD.idregistroiva LOOP
+    	UPDATE registroiva SET baseimp = mrecord.suma, iva = mrecord.sumaiva WHERE idregistroiva = OLD.idregistroiva;
+    END LOOP;
+    RETURN OLD;
+  END IF;
+END;
+'    LANGUAGE plpgsql;
+
+
+\echo -n ':: Disparador cambio tipo IVA ... '
+CREATE TRIGGER cambiadoivat
+   AFTER INSERT OR UPDATE OR DELETE ON iva
+   FOR EACH ROW
+   EXECUTE PROCEDURE cambiadoiva(); 
 
 
 -- ================================== FIN PARCHE. ACTUALIZACION  =======================
@@ -94,9 +246,9 @@ DECLARE
 BEGIN
 	SELECT INTO as * FROM configuracion WHERE nombre = ''DatabaseRevision'';
 	IF FOUND THEN
-		UPDATE CONFIGURACION SET valor = ''0.12.1-0001'' WHERE nombre = ''DatabaseRevision'';
+		UPDATE CONFIGURACION SET valor = ''0.12.1-0003'' WHERE nombre = ''DatabaseRevision'';
 	ELSE
-		INSERT INTO configuracion (nombre, valor) VALUES (''DatabaseRevision'', ''0.12.1-0001'');
+		INSERT INTO configuracion (nombre, valor) VALUES (''DatabaseRevision'', ''0.12.1-0003'');
 	END IF;
 	RETURN 0;
 END;
--- bulmages/installbulmages/dbmodels/crear/bulmacont/bulmacont_schema.sql
+++ bulmages/installbulmages/dbmodels/crear/bulmacont/bulmacont_schema.sql
@@ -807,6 +807,7 @@ END;
 
 
 \echo -n ':: Funcion nivel de codigo de cuenta ... '
+\echo -n ':: Usamos la longitud del codigo para determinarlo ya que los sistemas contables usan esa notacion. Podria usarse el nivel en el arbol de cuentas y seria mas general y correcto'
 CREATE FUNCTION nivel(character varying) RETURNS integer
     AS '
 DECLARE
@@ -1251,9 +1252,8 @@ BEGIN
 END;
 '    LANGUAGE plpgsql;
 
-
-\echo -n ':: Funcion aumenta valor ... '
-CREATE FUNCTION aumenta_valor() RETURNS "trigger"
+\echo -n ':: Funcion tratamiento de acumulados ... '
+CREATE FUNCTION trataacumulados() RETURNS "trigger"
     AS '
 DECLARE
     cta int4;
@@ -1261,46 +1261,54 @@ DECLARE
     ctar RECORD;
     ccostr RECORD;
 BEGIN
-	UPDATE cuenta SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idcuenta = NEW.idcuenta;
-	UPDATE c_coste SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idc_coste = NEW.idc_coste;
-	IF NEW.idcuenta IS NOT NULL THEN
-		UPDATE acumulado_canal SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idcuenta = NEW.idcuenta AND idcanal = NEW.idcanal;
-	END IF;
-	cta := NEW.idcuenta;
-	ccost := NEW.idc_coste;
-	-- RAISE NOTICE '' Se ha lanzado la funcion aumenta_valor()'';
-	SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccost;
+  -- Si se trata de una insercion hacemos los calculos con el campo NEW
+  IF TG_OP = ''INSERT'' THEN
+	  UPDATE cuenta SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idcuenta = NEW.idcuenta;
+	  UPDATE c_coste SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idc_coste = NEW.idc_coste;
+	  IF NEW.idcuenta IS NOT NULL THEN
+		  UPDATE acumulado_canal SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idcuenta = NEW.idcuenta AND idcanal = NEW.idcanal;
+	  END IF;
+	  cta := NEW.idcuenta;
+	  ccost := NEW.idc_coste;
+	  -- RAISE NOTICE '' Se ha lanzado la funcion aumenta_valor()'';
+	  SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccost;
+	  WHILE FOUND LOOP
+		  SELECT INTO ctar * FROM cuenta WHERE idcuenta = cta;
+		  WHILE FOUND LOOP
+		  -- RAISE NOTICE '' Cuenta % Centro Coste %'', ctar.idcuenta, ccostr.idc_coste;
+		  UPDATE acumulado_c_coste SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idc_coste = ccostr.idc_coste AND idcuenta = ctar.idcuenta;
+		  SELECT INTO ctar * FROM cuenta WHERE idcuenta = ctar.padre;
+		  END LOOP;
+		  SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccostr.padre;
+	  END LOOP;
+      RETURN NEW;
+  END IF;
+
+  -- Si se trata de un borrado se hacen los calculos con el campo OLD
+  IF TG_OP = ''DELETE'' THEN
+    UPDATE cuenta SET debe = debe - OLD.debe, haber = haber - OLD.haber WHERE idcuenta = OLD.idcuenta;
+    UPDATE c_coste SET debe = debe - OLD.debe, haber = haber - OLD.haber WHERE idc_coste = OLD.idc_coste;
+    IF OLD.idcuenta IS NOT NULL THEN
+	UPDATE acumulado_canal SET debe= debe - OLD.debe, haber =haber - OLD.haber WHERE idcuenta = OLD.idcuenta AND idcanal = OLD.idcanal;
+    END IF;
+    cta := OLD.idcuenta;
+    ccost := OLD.idc_coste;
+    -- RAISE NOTICE '' Se ha lanzado la funcion disminuye_valor()'';
+    SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccost;
+    WHILE FOUND LOOP
+	SELECT INTO ctar * FROM cuenta WHERE idcuenta = cta;
 	WHILE FOUND LOOP
-		SELECT INTO ctar * FROM cuenta WHERE idcuenta = cta;
-		WHILE FOUND LOOP
-		-- RAISE NOTICE '' Cuenta % Centro Coste %'', ctar.idcuenta, ccostr.idc_coste;
-		UPDATE acumulado_c_coste SET debe = debe + NEW.debe, haber = haber + NEW.haber WHERE idc_coste = ccostr.idc_coste AND idcuenta = ctar.idcuenta;
-		SELECT INTO ctar * FROM cuenta WHERE idcuenta = ctar.padre;
-		END LOOP;
-		SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccostr.padre;
+	    -- RAISE NOTICE '' Cuenta % Centro Coste %'', ctar.idcuenta, ccostr.idc_coste;
+	    UPDATE acumulado_c_coste SET debe = debe - OLD.debe, haber = haber -OLD.haber WHERE idc_coste = ccostr.idc_coste AND idcuenta = ctar.idcuenta;
+	    SELECT INTO ctar * FROM cuenta WHERE idcuenta = ctar.padre;
 	END LOOP;
-    RETURN NEW;
-END;
-'
-    LANGUAGE plpgsql;
+	SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccostr.padre;
+    END LOOP;
+    -- RAISE NOTICE '' disminuye_valor: Finaliza el algoritmo. '';
+    RETURN OLD;
+  END IF;
 
-
-\echo -n ':: Disparador nuevo apunte aumente valor ... '
-CREATE TRIGGER nuevo_apunte
-    AFTER INSERT ON apunte
-    FOR EACH ROW
-    EXECUTE PROCEDURE aumenta_valor();
-
-
-\echo -n ':: Funcion disminuye valor ... '
-CREATE FUNCTION disminuye_valor() RETURNS "trigger"
-    AS '
-DECLARE
-    cta int4;
-    ccost int4;
-    ctar RECORD;
-    ccostr RECORD;
-BEGIN
+  IF TG_OP = ''UPDATE'' THEN
     IF NEW.debe <> OLD.debe OR NEW.haber <> OLD.haber THEN
 	-- RAISE NOTICE ''disminuye_valor: debe antiguo %, debe nuevo %'', OLD.debe, NEW.debe;
 	UPDATE cuenta SET debe = debe - OLD.debe, haber = haber - OLD.haber + NEW.haber WHERE idcuenta = OLD.idcuenta;
@@ -1345,58 +1353,18 @@ BEGIN
 	-- RAISE NOTICE '' disminuye_valor: Finaliza el algoritmo. '';
     END IF;
     RETURN NEW;
+  END IF;
+
 END;
 '
     LANGUAGE plpgsql;
 
 
-\echo -n ':: Disparador nuevo apunte disminuye valor ... '
-CREATE TRIGGER nuevo_apunte1
-    AFTER UPDATE ON apunte
+\echo -n ':: Disparador nuevo apunte aumente valor ... '
+CREATE TRIGGER trataacumuladost
+    AFTER INSERT OR UPDATE OR DELETE ON apunte
     FOR EACH ROW
-    EXECUTE PROCEDURE disminuye_valor();
-
-
-\echo -n ':: Funcion disminuye valor ... '
-CREATE FUNCTION disminuye_valor1() RETURNS "trigger"
-    AS '
-DECLARE
-    cta int4;
-    ccost int4;
-    ctar RECORD;
-    ccostr RECORD;
-BEGIN
-    UPDATE cuenta SET debe = debe - OLD.debe, haber = haber - OLD.haber WHERE idcuenta = OLD.idcuenta;
-    UPDATE c_coste SET debe = debe - OLD.debe, haber = haber - OLD.haber WHERE idc_coste = OLD.idc_coste;
-    IF OLD.idcuenta IS NOT NULL THEN
-	UPDATE acumulado_canal SET debe= debe - OLD.debe, haber =haber - OLD.haber WHERE idcuenta = OLD.idcuenta AND idcanal = OLD.idcanal;
-    END IF;
-    cta := OLD.idcuenta;
-    ccost := OLD.idc_coste;
-    -- RAISE NOTICE '' Se ha lanzado la funcion disminuye_valor()'';
-    SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccost;
-    WHILE FOUND LOOP
-	SELECT INTO ctar * FROM cuenta WHERE idcuenta = cta;
-	WHILE FOUND LOOP
-	    -- RAISE NOTICE '' Cuenta % Centro Coste %'', ctar.idcuenta, ccostr.idc_coste;
-	    UPDATE acumulado_c_coste SET debe = debe - OLD.debe, haber = haber -OLD.haber WHERE idc_coste = ccostr.idc_coste AND idcuenta = ctar.idcuenta;
-	    SELECT INTO ctar * FROM cuenta WHERE idcuenta = ctar.padre;
-	END LOOP;
-	SELECT INTO ccostr * FROM c_coste WHERE idc_coste = ccostr.padre;
-    END LOOP;
-    -- RAISE NOTICE '' disminuye_valor: Finaliza el algoritmo. '';
-    RETURN OLD;
-END;
-'
-    LANGUAGE plpgsql;
-
-
-\echo -n ':: Disoparador nuevo apunte disminuye valor ... '
-CREATE TRIGGER nuevo_apunte2
-    AFTER DELETE ON apunte
-    FOR EACH ROW
-    EXECUTE PROCEDURE disminuye_valor1();
-
+    EXECUTE PROCEDURE trataacumulados();
 
 \echo -n ':: Funcion crea cuenta ... '
 CREATE FUNCTION creacuenta() RETURNS "trigger"
@@ -1981,40 +1949,30 @@ CREATE OR REPLACE FUNCTION cambiadoiva() RETURNS "trigger"
 DECLARE
     mrecord RECORD;
 BEGIN
+  IF TG_OP = ''INSERT'' OR TG_OP =''UPDATE'' THEN
     FOR mrecord IN SELECT SUM(baseiva) AS suma, SUM(ivaiva) AS sumaiva FROM iva WHERE iva.idregistroiva = NEW.idregistroiva LOOP
     	UPDATE registroiva SET baseimp = mrecord.suma, iva = mrecord.sumaiva WHERE idregistroiva = NEW.idregistroiva;
     END LOOP;
     RETURN NEW;
+  END IF;
+
+  IF TG_OP = ''DELETE'' THEN
+    FOR mrecord IN SELECT SUM(baseiva) AS suma, SUM(ivaiva) AS sumaiva FROM iva WHERE iva.idregistroiva = OLD.idregistroiva LOOP
+    	UPDATE registroiva SET baseimp = mrecord.suma, iva = mrecord.sumaiva WHERE idregistroiva = OLD.idregistroiva;
+    END LOOP;
+    RETURN OLD;
+  END IF;
 END;
 '    LANGUAGE plpgsql;
 
 
 \echo -n ':: Disparador cambio tipo IVA ... '
-CREATE TRIGGER civa
-   AFTER INSERT OR UPDATE ON iva
+CREATE TRIGGER cambiadoivat
+   AFTER INSERT OR UPDATE OR DELETE ON iva
    FOR EACH ROW
    EXECUTE PROCEDURE cambiadoiva();  
    
    
-\echo -n ':: Funcion cambiado IVA ... '
-CREATE OR REPLACE FUNCTION cambiadoivad() RETURNS "trigger"
-    AS '
-DECLARE
-    mrecord RECORD;
-BEGIN
-    FOR mrecord IN SELECT SUM(baseiva) AS suma, SUM(ivaiva) AS sumaiva FROM iva WHERE iva.idregistroiva = OLD.idregistroiva LOOP
-    	UPDATE registroiva SET baseimp = mrecord.suma, iva = mrecord.sumaiva WHERE idregistroiva = OLD.idregistroiva;
-    END LOOP;
-    RETURN OLD;
-END;
-'    LANGUAGE plpgsql;
-
-
-\echo -n ':: Disparador cambiado IVA ... '
-CREATE TRIGGER civad
-   AFTER DELETE ON iva
-   FOR EACH ROW
-   EXECUTE PROCEDURE cambiadoivad();     
 
 -- ******************************************************
 -- FIN DEL APARTADO DE COMPROBACIONES.
@@ -2031,16 +1989,16 @@ DECLARE
 BEGIN
 	SELECT INTO as * FROM configuracion WHERE nombre = ''DatabaseRevision'';
 	IF FOUND THEN
-		UPDATE CONFIGURACION SET valor = ''0.12.1-0001'' WHERE nombre = ''DatabaseRevision'';
+		UPDATE CONFIGURACION SET valor = ''0.12.1-0003'' WHERE nombre = ''DatabaseRevision'';
 	ELSE
-		INSERT INTO configuracion (nombre, valor) VALUES (''DatabaseRevision'', ''0.12.1-0001'');
+		INSERT INTO configuracion (nombre, valor) VALUES (''DatabaseRevision'', ''0.12.1-0003'');
 	END IF;
 	RETURN 0;
 END;
 '   LANGUAGE plpgsql;
 SELECT actualizarevision();
 DROP FUNCTION actualizarevision() CASCADE;
-\echo "Actualizada la revision de la base de datos a la version 0.12.1"
+\echo "Actualizada la revision de la base de datos a la version 0.12.2"
 
 \echo -n ':: '
 COMMENT ON SCHEMA public IS 'Standard public schema';
