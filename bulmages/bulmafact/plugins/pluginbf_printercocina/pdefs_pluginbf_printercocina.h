2284627f6975cbe896f1373541269f55498ace34
Tomeu Borras <tborras@gmail.com>
2010-07-25 08:55:49 +0000

    No habia agregado los archivos

---
+++ bulmages/bulmafact/plugins/pluginbf_printercocina/pdefs_pluginbf_printercocina.h
@@ -0,0 +1,21 @@
+#ifndef PDEFS_PLUGINBF_PRINTERCOCINA_H
+#define PDEFS_PLUGINBF_PRINTERCOCINA_H
+
+
+#ifdef WIN32
+
+#ifdef PLUGINBF_PRINTERCOCINA
+#define PLUGINBF_PRINTERCOCINA_EXPORT __declspec(dllexport)
+#else
+#define PLUGINBF_PRINTERCOCINA_EXPORT __declspec(dllimport)
+#endif
+
+#else
+
+#define PLUGINBF_PRINTERCOCINA_EXPORT
+
+#endif
+
+
+#endif
+
