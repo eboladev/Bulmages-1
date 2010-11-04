3651c008d32b4be0e09281696d55aa5cebdb4a06
Tomeu Borras <tborras@gmail.com>
2010-11-04 21:11:55 +0000

    CAMBIO PELIGROSO. DANGER svn st./fllite  He cambiado el bgtrml2pdf del 2003 por la nueva version que habia hecha del 2009 (nada que ver). Lo he probado un par de veces y parece que parsea bien y seguramente arregla muchos errores como los numeros de pagina y demas. Pero por otra parte tambien es posible que alguna de las plantillas que tengais hechas deje de funcionar y necesite algunos retoques. O rehacerla de P a PA

--- bulmages/installbulmages/bgtrml2pdf/bgcolor.py
+++ bulmages/installbulmages/bgtrml2pdf/bgcolor.py
@@ -1,19 +1,24 @@
-# trml2pdf - An RML to PDF converter
-# Copyright (C) 2003, Fabien Pinckaers, UCL, FSA
+# -*- encoding: utf-8 -*-
+##############################################################################
 #
-# This library is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
 #
-# This library is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
 #
-# You should have received a copy of the GNU Lesser General Public
-# License along with this library; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
 
 from reportlab.lib import colors
 import re
@@ -24,13 +29,18 @@ regex_t = re.compile('\(([0-9\.]*),([0-9\.]*),([0-9\.]*)\)')
 regex_h = re.compile('#([0-9a-zA-Z][0-9a-zA-Z])([0-9a-zA-Z][0-9a-zA-Z])([0-9a-zA-Z][0-9a-zA-Z])')
 
 def get(col_str):
-	global allcols
-	if col_str in allcols.keys():
-		return allcols[col_str]
-	res = regex_t.search(col_str, 0)
-	if res:
-		return (float(res.group(1)),float(res.group(2)),float(res.group(3)))
-	res = regex_h.search(col_str, 0)
-	if res:
-		return tuple([ float(int(res.group(i),16))/255 for i in range(1,4)])
-	return colors.red
+    if col_str == None:
+        col_str = ''
+    global allcols
+    if col_str in allcols.keys():
+        return allcols[col_str]
+    res = regex_t.search(col_str, 0)
+    if res:
+        return (float(res.group(1)),float(res.group(2)),float(res.group(3)))
+    res = regex_h.search(col_str, 0)
+    if res:
+        return tuple([ float(int(res.group(i),16))/255 for i in range(1,4)])
+    return colors.red
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
+
--- bulmages/installbulmages/bgtrml2pdf/bgutils.py
+++ bulmages/installbulmages/bgtrml2pdf/bgutils.py
@@ -1,3 +1,25 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OpenERP, Open Source Management Solution
+#    Copyright (C) 2004-2009 Tiny SPRL (<http://tiny.be>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
 # trml2pdf - An RML to PDF converter
 # Copyright (C) 2003, Fabien Pinckaers, UCL, FSA
 #
@@ -17,70 +39,169 @@
 
 import re
 import reportlab
+from lxml import etree
+import copy
+#import tools
+
+_regex = re.compile('\[\[(.+?)\]\]')
 
 
-#
-# Change this to UTF-8 if you plan tu use Reportlab's UTF-8 support
-#
-encoding = 'UTF-8'
-#encoding = 'utf-8'
-#encoding = 'latin1'
-#encoding = 'cp1252'
+def ustr(value):
+    """This method is similar to the builtin `str` method, except
+    it will return Unicode string.                               
 
+    @param value: the value to convert
+
+    @rtype: unicode
+    @return: unicode string
+    """                    
+
+    if isinstance(value, unicode):
+        return value              
+
+    if hasattr(value, '__unicode__'):
+        return unicode(value)        
+
+    if not isinstance(value, str):
+        value = str(value)        
+
+    try: # first try utf-8
+        return unicode(value, 'utf-8')
+    except:                           
+        pass                          
+
+    try: # then extened iso-8858
+        return unicode(value, 'iso-8859-15')
+    except:                                 
+        pass                                
+
+    # else use default system locale
+    from locale import getlocale    
+    return unicode(value, getlocale()[1])
+
+
+
+
+def str2xml(s):
+    return s.replace('&', '&amp;').replace('<', '&lt;').replace('>', '&gt;')
+
+def xml2str(s):
+    return s.replace('&amp;','&').replace('&lt;','<').replace('&gt;','>')
+
+def _child_get(node, self=None, tagname=None):
+    for n in node:
+        if self and self.localcontext and n.get('rml_loop', False):
+            oldctx = self.localcontext
+
+            for ctx in eval(n.get('rml_loop'),{}, self.localcontext):
+                self.localcontext.update(ctx)
+                if (tagname is None) or (n.tag==tagname):
+                    if n.get('rml_except', False):
+                        try:
+                            eval(n.get('rml_except'), {}, self.localcontext)
+                        except:
+                            continue
+                    if n.get('rml_tag'):
+                        try:
+                            (tag,attr) = eval(n.get('rml_tag'),{}, self.localcontext)
+                            n2 = copy.deepcopy(n)
+                            n2.tag = tag
+                            n2.attrib.update(attr)
+                            yield n2
+                        except:
+                            yield n
+                    else:
+                        yield n
+            self.localcontext = oldctx
+            continue
+        if self and self.localcontext and n.get('rml_except', False):
+            try:
+                eval(n.get('rml_except'), {}, self.localcontext)
+            except:
+                continue
+        if self and self.localcontext and n.get('rml_tag', False):
+            try:
+                (tag,attr) = eval(n.get('rml_tag'),{}, self.localcontext)
+                n2 = copy.deepcopy(n)
+                n2.tag = tag
+                n2.attrib.update(attr or {})
+                yield n2
+                tagname = ''
+            except:
+                pass
+        if (tagname is None) or (n.tag==tagname):
+            yield n
+
+def _process_text(self, txt):
+        if not self.localcontext:
+            return str2xml(txt)
+        if not txt:
+            return ''
+        result = ''
+        sps = _regex.split(txt)
+        while sps:
+            # This is a simple text to translate
+            result += self.localcontext.get('translate', lambda x:x)(sps.pop(0))
+            if sps:
+                try:
+                    txt = eval(sps.pop(0),self.localcontext)
+                except:
+                    pass
+                if type(txt)==type('') or type(txt)==type(u''):
+                    txt2 = str2xml(txt)
+                    result += ustr(txt2)
+                elif (txt is not None) and (txt is not False):
+                    result += str(txt)
+        return result
 
 def text_get(node):
-	rc = ''
-	for node in node.childNodes:
-		if node.nodeType == node.TEXT_NODE:
-			rc = rc + node.data
-	return rc
+    rc = ''
+    for node in node.getchildren():
+            rc = rc + ustr(node.text)
+    return rc
 
 units = [
-	(re.compile('^(-?[0-9\.]+)\s*in$'), reportlab.lib.units.inch),
-	(re.compile('^(-?[0-9\.]+)\s*cm$'), reportlab.lib.units.cm),  
-	(re.compile('^(-?[0-9\.]+)\s*mm$'), reportlab.lib.units.mm),
-	(re.compile('^(-?[0-9\.]+)\s*$'), 1)
+    (re.compile('^(-?[0-9\.]+)\s*in$'), reportlab.lib.units.inch),
+    (re.compile('^(-?[0-9\.]+)\s*cm$'), reportlab.lib.units.cm),
+    (re.compile('^(-?[0-9\.]+)\s*mm$'), reportlab.lib.units.mm),
+    (re.compile('^(-?[0-9\.]+)\s*$'), 1)
 ]
 
 def unit_get(size):
-	global units
-	for unit in units:
-		res = unit[0].search(size, 0)
-		if res:
-			return unit[1]*float(res.group(1))
-	return False
+    global units
+    if size:
+        for unit in units:
+            res = unit[0].search(size, 0)
+            if res:
+                return unit[1]*float(res.group(1))
+    return False
 
 def tuple_int_get(node, attr_name, default=None):
-	if not node.hasAttribute(attr_name):
-		return default
-	res = [int(x) for x in node.getAttribute(attr_name).split(',')]
-	return res
+    if not node.get(attr_name):
+        return default
+    res = [int(x) for x in node.get(attr_name).split(',')]
+    return res
 
 def bool_get(value):
-	return (str(value)=="1") or (value.lower()=='yes')
-
-def strSafe(param):
-	if isinstance(param,str):
-        	return param
-        if isinstance(param,unicode):
-        	return param.encode(encoding)
-        return str(param)
+    return (str(value)=="1") or (value.lower()=='yes')
 
 def attr_get(node, attrs, dict={}):
-	res = {}
-	for name in attrs:
-		if node.hasAttribute(name):
-			res[name] =  unit_get(node.getAttribute(name))
-	for key in dict:
-		if node.hasAttribute(key):
-			if dict[key]=='str':
-				res[key] = strSafe(node.getAttribute(key))
-			elif dict[key]=='bool':
-				res[key] = bool_get(node.getAttribute(key))
-			elif dict[key]=='int':
-				res[key] = int(node.getAttribute(key))
-			elif dict[key]=='float':
-				res[key] = float(node.getAttribute(key))
-			elif dict[key]=='unit':
-				res[key] = unit_get(node.getAttribute(key))
-	return res
+    res = {}
+    for name in attrs:
+        if node.get(name):
+            res[name] = unit_get(node.get(name))
+    for key in dict:
+        if node.get(key):
+            if dict[key]=='str':
+                res[key] = str(node.get(key))
+            elif dict[key]=='bool':
+                res[key] = bool_get(node.get(key))
+            elif dict[key]=='int':
+                res[key] = int(node.get(key))
+            elif dict[key]=='unit':
+                res[key] = unit_get(node.get(key))
+            elif dict[key] == 'float' : 
+                res[key] = float(node.get(key))
+    return res
+
+# vim:expandtab:smartindent:tabstop=4:softtabstop=4:shiftwidth=4:
