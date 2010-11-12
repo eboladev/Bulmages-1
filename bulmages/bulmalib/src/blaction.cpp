cdcd156eb9ee069aed2bad5390c88c0f26f292a7
Tomeu Borras <tborras@gmail.com>
2010-11-12 20:23:01 +0000

    Agrego la clase BlAction que sirve para crear entradas de menu que pasan su activacion por salida de plugins en lugar de signal slot

---
+++ bulmages/bulmalib/src/blaction.cpp
@@ -0,0 +1,78 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Tomeu Borras Riera                              *
+ *   tborras@conetxia.com                                                  *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include "blaction.h"
+#include "blfunctions.h"
+#include "blplugins.h"
+
+///
+/**
+\param parent
+\param f
+**/
+BlAction::BlAction ( const QString &text, QWidget *parent ) : QAction ( text,parent ), BlMainCompanyPointer()
+{
+    blDebug ( "BlAction::BlAction", 0 );
+    connect (this, SIGNAL(triggered(bool)), this, SLOT(actionTriggered(bool)));
+    blDebug ( "END BlAction::BlAction", 0 );
+}
+
+
+///
+/**
+\param emp
+\param parent
+\param f
+**/
+BlAction::BlAction ( BlMainCompany *emp,const QString &text, QWidget *parent ) : QAction ( text, 					parent ), BlMainCompanyPointer ( emp )
+{
+    blDebug ( "BlAction::BlAction", 0 );
+    connect (this, SIGNAL(triggered(bool)), this, SLOT(actionTriggered(bool)));
+    blDebug ( "END BlAction::BlAction", 0 );
+}
+
+
+///
+/**
+**/
+BlAction::~BlAction()
+{
+    blDebug ( "BlAction::~BlAction", 0 );
+    blDebug ( "END BlAction::~BlAction", 0 );
+}
+
+void BlAction::actionTriggered(bool trigg) {
+    g_plugins->lanza("BlAction_triggered", this);
+}
+
+
+
+/*
+/// Desde los plugins (aun no se bien pq) no se pueden crear objetos definidos en la libreria. De hecho compila y linka pero acaba dando un error
+/// De procedimiento no encontrado. El problema parecer ser debido a la reserva de memoria asi que si se hace el new dentro de la libreria llamante no
+/// da problemas. Por eso este procedimiento.
+/// Sino que se llama desde multiples partes del sistema.
+BlAction * newBlAction ( BlMainCompany *emp, QWidget *parent, Qt::WFlags f )
+{
+    BlAction *h = new BlTooloButton ( emp, parent, f );
+    return h;
+}
+
+*/
---
+++ bulmages/bulmalib/src/blaction.h
@@ -0,0 +1,48 @@
+/****************************************************************************
+**
+** Copyright (C) 2010-2010 Tomeu Borrás Riera. All rights reserved.
+**
+** This file is part of the documentation of the Qt Toolkit.
+**
+** This file may be used under the terms of the GNU General Public
+** License version 2.0 as published by the Free Software Foundation
+** and appearing in the file LICENSE.GPL included in the packaging of
+** this file.  Please review the following information to ensure GNU
+** General Public Licensing requirements will be met:
+** http://www.trolltech.com/products/qt/opensource.html
+**
+** If you are unsure which license is appropriate for your use, please
+** review the following information:
+** http://www.trolltech.com/products/qt/licensing.html or contact the
+** sales department at sales@trolltech.com.
+**
+** This file is provided AS IS with NO WARRANTY OF ANY KIND, INCLUDING THE
+** WARRANTY OF DESIGN, MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE.
+**
+****************************************************************************/
+
+#ifndef BLACTION_H
+#define BLACTION_H
+
+#include <QToolButton>
+#include <QAction>
+#include "blmaincompanypointer.h"
+
+
+class BL_EXPORT BlAction : public QAction, public BlMainCompanyPointer
+{
+    Q_OBJECT
+
+public:
+    BlAction ( const QString &text, QWidget *parent = 0 );
+    BlAction ( BlMainCompany *,const QString &text, QWidget *parent = 0);
+    virtual ~BlAction();
+public slots:
+    virtual void actionTriggered(bool trigg);
+    
+};
+
+// BlAction * newBlAction(BlMainCompany *, QWidget *parent = 0, Qt::WFlags f = 0 );
+
+
+#endif
