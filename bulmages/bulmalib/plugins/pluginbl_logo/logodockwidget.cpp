cdb82b3e8ae7adbb5dc8117a80b2ca416d02f573
Fco. Javier M.C <fcojavmc@todo-redes.com>
2010-11-24 20:08:41 +0000

    Perdon, faltaba subir estos archivos.
    *********************************
    *          Todo-Redes           *
    *        C/.Begonia, 12         *
    *   07010 Palma de Mallorca.    *
    *      Tel: +34-971499071       *
    *   http://www.todo-redes.com   *
    *  comercial(en)todo-redes.com  *
    *********************************

---
+++ bulmages/bulmalib/plugins/pluginbl_logo/logodockwidget.cpp
@@ -0,0 +1,55 @@
+/***************************************************************************
+ *   Copyright (C) 2010 by Fco. Javier M. C.                               *
+ *   fcojavmc@todo-redes.com                                               *
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ *   This program is distributed in the hope that it will be useful,       *
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of        *
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the         *
+ *   GNU General Public License for more details.                          *
+ *                                                                         *
+ *   You should have received a copy of the GNU General Public License     *
+ *   along with this program; if not, write to the                         *
+ *   Free Software Foundation, Inc.,                                       *
+ *   59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.             *
+ ***************************************************************************/
+
+#include <QLabel>
+#include <QSize>
+
+#include "logodockwidget.h"
+
+
+LogoDockWidget::LogoDockWidget( const QString & title, QWidget * parent, const QString &name , Qt::WindowFlags flags ) : BlDockWidget ( title, parent, name, flags )
+{
+    m_label = new QLabel();
+    m_label->setFrameStyle(QFrame::Panel | QFrame::Sunken);
+    m_label->setAlignment(Qt::AlignBottom | Qt::AlignRight);
+    m_label->setScaledContents(true);
+    m_label->setMinimumSize(100, 100);
+
+    setWidget ( m_label );
+}
+
+
+LogoDockWidget::~LogoDockWidget()
+{
+}
+
+  
+void LogoDockWidget::resizeEvent(QResizeEvent *event)
+{
+}
+
+
+void LogoDockWidget::setPixmap(QPixmap pixmap)
+{
+  m_originalPixmap = pixmap;
+  m_label->setPixmap(m_originalPixmap);
+}
+
+
