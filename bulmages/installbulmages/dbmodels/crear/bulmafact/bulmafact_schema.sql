45d29f5965244fcf4ffcd3ebfcb2485a6a0f97f1
Daniel Ripoll <info@danielripoll.es>
2011-11-29 12:42:15 +0100

    Añado nuevo query a los schemas para comrpobar y crear PL/PGSQL si no existe.

--- bulmages/installbulmages/dbmodels/crear/bulmafact/bulmafact_schema.sql
+++ bulmages/installbulmages/dbmodels/crear/bulmafact/bulmafact_schema.sql
@@ -42,7 +42,31 @@ CREATE FUNCTION plpgsql_call_handler() RETURNS language_handler
     LANGUAGE c;
     
 \echo -n ':: Establecemos el lenguaje de procedimientos ... '
-CREATE TRUSTED PROCEDURAL LANGUAGE plpgsql HANDLER plpgsql_call_handler;
+--~ CREATE TRUSTED PROCEDURAL LANGUAGE plpgsql HANDLER plpgsql_call_handler;
+
+CREATE OR REPLACE FUNCTION create_language_plpgsql()
+ RETURNS BOOLEAN AS $$
+     CREATE LANGUAGE plpgsql;
+     SELECT TRUE;
+ $$ LANGUAGE SQL;
+
+SELECT CASE WHEN NOT
+     (
+         SELECT  TRUE AS exists
+         FROM    pg_language
+         WHERE   lanname = 'plpgsql'
+         UNION
+         SELECT  FALSE AS exists
+         ORDER BY exists DESC
+         LIMIT 1
+     )
+ THEN
+     create_language_plpgsql()
+ ELSE
+     FALSE
+ END AS plpgsql_created;
+
+DROP FUNCTION create_language_plpgsql();
 
 
 -- NOTACION:
