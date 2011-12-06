23b9bab08f4f50f3d90f21207dae8deb5cae7eaa
Daniel Ripoll <info@danielripoll.es>
2011-12-06 13:40:32 +0100

    Corrijo el ident incorrecto en functions.py (no hice el commit)

--- bulmages/bulmasetup/functions.py
+++ bulmages/bulmasetup/functions.py
@@ -44,5 +44,5 @@ class multios():
         
     def grep(self,word,desiredpath):
         for line in open(desiredpath):
-         if word in line:
-           return line
+            if word in line:
+                return line
