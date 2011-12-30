61d4af55a7290c9fd319405403f15a8bf95eda91
Tomeu Borras <tborras@conetxia.com>
2011-12-30 09:33:47 +0100

    Arreglo un problema en el sql del plugin de demostracion de AutoForms que daba un error en bulmasetup

--- bulmages/bulmafact/plugins/pluginbf_biblioteca/dbmodels/pluginbf_biblioteca-rm.sql
+++ bulmages/bulmafact/plugins/pluginbf_biblioteca/dbmodels/pluginbf_biblioteca-rm.sql
@@ -1,13 +1,106 @@
-ALTER TABLE cliente DROP COLUMN idzcomercial;
+--
+-- Modificacion de campos y funciones de la BD para la adaptacion para el plugin de profesores
+--
+\echo "********* INICIADO FICHERO DE ESTRUCTURA DEL PLUGIN DE VARIACION TARIFA POR CANTIDAD *********"
 
-DROP TABLE zcomercial;
+\echo ":: Establecemos los mensajes minimos a avisos y otros parametros ... "
+\echo -n ":: "
+SET client_min_messages TO WARNING;
+SET log_min_messages TO WARNING;
+-- SET log_error_verbosity TO TERSE;
+BEGIN;
 
-DROP TABLE prestamo;
+--
+-- Funcion auxiliar para borrar funciones limpiamente
+--
+create or replace function drop_if_exists_table (text) returns INTEGER AS '
+DECLARE
+    tbl_name ALIAS FOR $1;
+BEGIN
+    IF (select count(*) from pg_tables where tablename=$1) THEN
+        EXECUTE ''DROP TABLE '' || $1;
+        RETURN 1;
+    END IF;
+    
+    RETURN 0;
+END;
+'
+language 'plpgsql';
 
-DROP TABLE libro;
+create or replace function drop_if_exists_proc (text,text) returns INTEGER AS '
+DECLARE
+proc_name ALIAS FOR $1;
+proc_params ALIAS FOR $2;
+BEGIN
+IF (select count(*) from pg_proc where proname=$1) THEN
+ EXECUTE ''DROP FUNCTION '' || $1 || ''(''||$2||'') CASCADE'';
+RETURN 1;
+END IF;
+RETURN 0;
+END;
+'
+language 'plpgsql';
 
-DROP TABLE socio;
+DROP FUNCTION IF EXISTS actualizacantrecibo_insert() CASCADE;
+DROP FUNCTION IF EXISTS actualizacantrecibo_update() CASCADE;
+DROP FUNCTION IF EXISTS actualizacantrecibo_delete() CASCADE;
 
-DROP TABLE autor;
+-- ========================== VARIACION DE TARIFA =======================
 
-DROP TABLE categorialibro;
+CREATE OR REPLACE FUNCTION aux() RETURNS INTEGER AS '
+DECLARE
+	rsa  RECORD;
+BEGIN
+
+    SELECT INTO rsa * FROM pg_tables  WHERE tablename=''zcomercial'';
+    IF FOUND THEN
+	ALTER TABLE cliente DROP COLUMN idzcomercial;
+
+	DROP TABLE zcomercial;
+
+	DROP TABLE prestamo;
+
+	DROP TABLE libro;
+
+	DROP TABLE socio;
+
+	DROP TABLE autor;
+
+	DROP TABLE categorialibro;
+    END IF;
+
+
+    RETURN 0;
+    
+END;
+'   LANGUAGE plpgsql;
+SELECT aux();
+DROP FUNCTION aux() CASCADE;
+\echo "Creamos las tablas para la gestion de biblioteca"
+
+-- ======================== Actualizada la revision de la base de datos a la version. =====================
+
+
+-- Agregamos nuevos parametros de configuracion.
+--
+CREATE OR REPLACE FUNCTION actualizarevision() RETURNS INTEGER AS '
+DECLARE
+	rsa  RECORD;
+BEGIN
+	SELECT INTO rsa * FROM configuracion WHERE nombre=''PluginBf_Biblioteca'';
+	IF FOUND THEN
+		DELETE FROM CONFIGURACION WHERE nombre=''PluginBf_Biblioteca'';
+	END IF;
+	RETURN 0;
+END;
+'   LANGUAGE plpgsql;
+
+SELECT actualizarevision();
+DROP FUNCTION actualizarevision() CASCADE;
+
+\echo "Actualizada la revision de la base de datos a la version"
+
+DROP FUNCTION drop_if_exists_table(text) CASCADE;
+DROP FUNCTION drop_if_exists_proc(text,text) CASCADE;
+
+COMMIT;
--- bulmages/bulmafact/plugins/pluginbf_biblioteca/dbmodels/pluginbf_biblioteca.sql
+++ bulmages/bulmafact/plugins/pluginbf_biblioteca/dbmodels/pluginbf_biblioteca.sql
@@ -1,72 +1,170 @@
-CREATE TABLE categorialibro
-(
-   idcategorialibro serial NOT NULL,
-   nomcategorialibro character varying(255) NOT NULL,
-   PRIMARY KEY (idcategorialibro)
-);
+--
+-- Modificacion de campos y funciones de la BD para la adaptacion para el plugin de profesores
+--
+\echo "********* INICIADO FICHERO DE ESTRUCTURA DE ASOCIACIONES DE PADRES DE ALUMNOS *********"
 
-CREATE TABLE autor
-(
-   idautor serial NOT NULL,
-   nomautor character varying(255) NOT NULL,
-   idpais integer,
-   fechanacimientoautor date,
-   PRIMARY KEY (idautor),
-   CONSTRAINT idpais FOREIGN KEY (idpais) REFERENCES pais (idpais)
-);
+\echo ":: Establecemos los mensajes minimos a avisos y otros parametros ... "
+\echo -n ":: "
+SET client_min_messages TO WARNING;
+SET log_min_messages TO WARNING;
+-- SET log_error_verbosity TO TERSE;
+BEGIN;
 
-CREATE TABLE socio
-(
-   idsocio serial NOT NULL,
-   nomsocio character varying(50),
-   apellido1socio character varying(50),
-   apellido2socio character varying(50),
-   fechanacimientosocio date,
-   telsocio character varying(20),
-   mailsocio character varying(50),
-   dirsocio character varying(100),
-   idprovincia integer,
-   CONSTRAINT idsocioprimarykey PRIMARY KEY (idsocio),
-   CONSTRAINT socioprovincia FOREIGN KEY (idprovincia) REFERENCES provincia (idprovincia) ON UPDATE NO ACTION ON DELETE NO ACTION
-);
-
-CREATE TABLE libro
-(
-   idlibro serial NOT NULL,
-   isbnlibro character varying(40) NOT NULL,
-   titulolibro character varying(200) NOT NULL,
-   idautor integer,
-   resumenlibro text,
-   idcategorialibro integer,
-   CONSTRAINT libroidliborprimarykey PRIMARY KEY (idlibro),
-   CONSTRAINT idcategorialibro_libro_externkey FOREIGN KEY (idcategorialibro) REFERENCES categorialibro (idcategorialibro) ON UPDATE NO ACTION ON DELETE NO ACTION,
-   CONSTRAINT autor_idautor_fkey FOREIGN KEY (idautor) REFERENCES autor (idautor) ON UPDATE NO ACTION ON DELETE NO ACTION
-);
+--
+-- Funcion auxiliar para borrar funciones limpiamente
+--
+create or replace function drop_if_exists_table (text) returns INTEGER AS '
+DECLARE
+tbl_name ALIAS FOR $1;
+BEGIN
+IF (select count(*) from pg_tables where tablename=$1) THEN
+ EXECUTE ''DROP TABLE '' || $1;
+RETURN 1;
+END IF;
+RETURN 0;
+END;
+'
+language 'plpgsql';
 
 
-CREATE TABLE prestamo
-(
-   idprestamo serial,
-   fechainprestamo date NOT NULL DEFAULT now(),
-   fechafinprestamo date NOT NULL DEFAULT now() +' @ 1 week',
-   idsocio integer NOT NULL,
-   idlibro integer NOT NULL,
-   devueltoprestamo boolean DEFAULT FALSE,
-   CONSTRAINT prestamo_idprestamo_p_key PRIMARY KEY (idprestamo),
-   CONSTRAINT socio_idsocio_f_key FOREIGN KEY (idsocio) REFERENCES socio (idsocio) ON UPDATE NO ACTION ON DELETE NO ACTION,
-   CONSTRAINT libro_idlibro_f_key FOREIGN KEY (idlibro) REFERENCES libro (idlibro) ON UPDATE NO ACTION ON DELETE NO ACTION
-);
+create or replace function drop_if_exists_proc (text,text) returns INTEGER AS '
+DECLARE
+proc_name ALIAS FOR $1;
+proc_params ALIAS FOR $2;
+BEGIN
+IF (select count(*) from pg_proc where proname=$1) THEN
+ EXECUTE ''DROP FUNCTION '' || $1 || ''(''||$2||'') CASCADE'';
+RETURN 1;
+END IF;
+RETURN 0;
+END;
+'
+language 'plpgsql';
+
+-- 
+-- Funcion que prepara la base de datos para funcionar como una AMPA
+-- 
+
+CREATE OR REPLACE FUNCTION aux() RETURNS INTEGER AS '
+DECLARE
+	rsa RECORD;
+BEGIN
+
+    SELECT INTO rsa * FROM pg_tables  WHERE tablename=''zcomercial'';
+    IF NOT FOUND THEN
 
 
-CREATE TABLE zcomercial (
-    idzcomercial SERIAL PRIMARY KEY,
-    nomzonacomercial VARCHAR(60),
-    avzcomercial VARCHAR(4)
-);
+	      CREATE TABLE categorialibro
+	      (
+		idcategorialibro serial NOT NULL,
+		nomcategorialibro character varying(255) NOT NULL,
+		PRIMARY KEY (idcategorialibro)
+	      );
 
-ALTER TABLE cliente ADD COLUMN idzcomercial integer REFERENCES zcomercial(idzcomercial) ;
+	      CREATE TABLE autor
+	      (
+		idautor serial NOT NULL,
+		nomautor character varying(255) NOT NULL,
+		idpais integer,
+		fechanacimientoautor date,
+		PRIMARY KEY (idautor),
+		CONSTRAINT idpais FOREIGN KEY (idpais) REFERENCES pais (idpais)
+	      );
 
-INSERT INTO zcomercial (nomzonacomercial, avzcomercial) VALUES ('Palma', 'PM');
-INSERT INTO zcomercial (nomzonacomercial, avzcomercial) VALUES ('Raiguer', 'RG');
-INSERT INTO zcomercial (nomzonacomercial, avzcomercial) VALUES ('Llevant', 'LL');
+	      CREATE TABLE socio
+	      (
+		idsocio serial NOT NULL,
+		nomsocio character varying(50),
+		apellido1socio character varying(50),
+		apellido2socio character varying(50),
+		fechanacimientosocio date,
+		telsocio character varying(20),
+		mailsocio character varying(50),
+		dirsocio character varying(100),
+		idprovincia integer,
+		CONSTRAINT idsocioprimarykey PRIMARY KEY (idsocio),
+		CONSTRAINT socioprovincia FOREIGN KEY (idprovincia) REFERENCES provincia (idprovincia) ON UPDATE NO ACTION ON DELETE NO ACTION
+	      );
+
+	      CREATE TABLE libro
+	      (
+		idlibro serial NOT NULL,
+		isbnlibro character varying(40) NOT NULL,
+		titulolibro character varying(200) NOT NULL,
+		idautor integer,
+		resumenlibro text,
+		idcategorialibro integer,
+		CONSTRAINT libroidliborprimarykey PRIMARY KEY (idlibro),
+		CONSTRAINT idcategorialibro_libro_externkey FOREIGN KEY (idcategorialibro) REFERENCES categorialibro (idcategorialibro) ON UPDATE NO ACTION ON DELETE NO ACTION,
+		CONSTRAINT autor_idautor_fkey FOREIGN KEY (idautor) REFERENCES autor (idautor) ON UPDATE NO ACTION ON DELETE NO ACTION
+	      );
+
+
+	      CREATE TABLE prestamo
+	      (
+		idprestamo serial,
+		fechainprestamo date NOT NULL DEFAULT now(),
+		fechafinprestamo date NOT NULL DEFAULT now() +' @ 1 week',
+		idsocio integer NOT NULL,
+		idlibro integer NOT NULL,
+		devueltoprestamo boolean DEFAULT FALSE,
+		CONSTRAINT prestamo_idprestamo_p_key PRIMARY KEY (idprestamo),
+		CONSTRAINT socio_idsocio_f_key FOREIGN KEY (idsocio) REFERENCES socio (idsocio) ON UPDATE NO ACTION ON DELETE NO ACTION,
+		CONSTRAINT libro_idlibro_f_key FOREIGN KEY (idlibro) REFERENCES libro (idlibro) ON UPDATE NO ACTION ON DELETE NO ACTION
+	      );
+
+
+	      CREATE TABLE zcomercial (
+		  idzcomercial SERIAL PRIMARY KEY,
+		  nomzonacomercial VARCHAR(60),
+		  avzcomercial VARCHAR(4)
+	      );
+
+	      ALTER TABLE cliente ADD COLUMN idzcomercial integer REFERENCES zcomercial(idzcomercial) ;
+
+	      INSERT INTO zcomercial (nomzonacomercial, avzcomercial) VALUES ('Palma', 'PM');
+	      INSERT INTO zcomercial (nomzonacomercial, avzcomercial) VALUES ('Raiguer', 'RG');
+	      INSERT INTO zcomercial (nomzonacomercial, avzcomercial) VALUES ('Llevant', 'LL');
+
+END IF;
+RETURN 0;
+
+
+
+END;
+'   LANGUAGE plpgsql;
+
+SELECT aux();
+DROP FUNCTION aux() CASCADE;
+\echo "Creamos la tabla profesor 333333."
+
+
+
+
+-- ======================== Actualizada la revision de la base de datos a la version. =====================
+
+
+-- Agregamos nuevos parametros de configuracion.
+--
+CREATE OR REPLACE FUNCTION actualizarevision() RETURNS INTEGER AS '
+DECLARE
+	rsa RECORD;
+BEGIN
+	SELECT INTO rsa * FROM configuracion WHERE nombre=''PluginBf_Biblioteca'';
+	IF FOUND THEN
+		UPDATE CONFIGURACION SET valor=''0.12.1-0003'' WHERE nombre=''PluginBf_Biblioteca'';
+	ELSE
+		INSERT INTO configuracion (nombre, valor) VALUES (''PluginBf_Biblioteca'', ''0.12.1-0003'');
+	END IF;
+	RETURN 0;
+END;
+'   LANGUAGE plpgsql;
+SELECT actualizarevision();
+DROP FUNCTION actualizarevision() CASCADE;
+\echo "Actualizada la revision de la base de datos a la version"
+
+DROP FUNCTION drop_if_exists_table(text) CASCADE;
+DROP FUNCTION drop_if_exists_proc(text,text) CASCADE;
+
+COMMIT;
 
