f1aedeb8d88a8219452433ca2a594b5adba72a03
Aron <auryn@wanadoo.es>
2012-02-14 18:01:42 +0100

    Nueva función blRemoveDirectory() para borrar directorios recursivamente. Cuando borra ficheros aprovecha blRemove(). -- Aron Galdón
    PostgreSQL, Qt4 and PHP developer

--- bulmages/bulmalib/src/blfunctions.cpp
+++ bulmages/bulmalib/src/blfunctions.cpp
@@ -1492,13 +1492,40 @@ bool blRemove(const QString &filetoremove )
     
 }
 
+
+/// Funci&oacute;n para borrar un directorio y todo su contenido
+bool blRemoveDirectory( const QString &direcorytoremove )
+{
+    BL_FUNC_DEBUG
+
+    bool res = true;
+    QString removeDirectory = QUrl ( direcorytoremove, QUrl::TolerantMode ).toString();
+    QDir dir ( removeDirectory );
+    if ( !dir.exists() )
+        return true;
+
+    foreach ( QFileInfo fi,
+              dir.entryInfoList ( QDir::AllDirs | QDir::Files | QDir::NoDotAndDotDot, QDir::DirsFirst ) ) {
+        if ( fi.isDir() )
+            res = blRemoveDirectory ( fi.absoluteFilePath () );
+        else
+            res = blRemove ( fi.absoluteFilePath() );
+
+        if ( !res )
+            return false;
+    }
+    res = dir.rmdir ( removeDirectory );
+
+    return res;
+}
+
+
 bool blMoveFile( const QString &oldName, const QString &newName )
 {
     ///Funcion para mover archivos, pudiendo mover entre particiones y con direcciones tolerantes multiplataforma.
     BL_FUNC_DEBUG
     QDir rootDir;
-    
-    
+
     // Transforma las direcciones en tolerantes, para  que funcione en totas las plataformas.
     QString oldFile = QUrl(oldName, QUrl::TolerantMode).toString();
     QString newFile = QUrl(newName, QUrl::TolerantMode).toString();
