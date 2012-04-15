ad175a89fd0ac91754d18b3877fffd2a753fc689
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2012-04-16 00:47:15 +0200

    Added some extra methods to see how the tests work.

--- bulmages/tests/test_initial.cpp
+++ bulmages/tests/test_initial.cpp
@@ -1,8 +1,59 @@
 #include "test_initial.h"
 
+void Initial::toUpper_data()
+{
+	QTest::addColumn<QString>("string");
+	QTest::addColumn<QString>("result");
+
+	QTest::newRow("all lower") << "hello" << "HELLO";
+	QTest::newRow("mixed")     << "Hello" << "HELLO";
+	QTest::newRow("all upper") << "HELLO" << "HELLO";
+}
+
 void Initial::toUpper() {
-	QString str = "Hello";
-	QCOMPARE(str.toUpper(), QString("HELLO"));
+	QFETCH(QString, string);
+	QFETCH(QString, result);
+	QCOMPARE(string.toUpper(), result);
+
+	QBENCHMARK {
+		int res = result.compare(string.toUpper());
+	}
+}
+
+void Initial::testGui_data() {
+	QTest::addColumn<QTestEventList>("events");
+	QTest::addColumn<QString>("expected");
+
+	QTestEventList list1;
+	list1.addKeyClick('a');
+	QTest::newRow("char") << list1 << "a";
+
+	QTestEventList list2;
+	list2.addKeyClick('a');
+	list2.addKeyClick(Qt::Key_Backspace);
+	QTest::newRow("there and back again") << list2 << "";
+
+	list1.addKeyClick('y');
+	QTest::newRow("char") << list1 << "ay";
+
+	list2.addKeyClick('a');
+	list2.addKeyClick(Qt::Key_Backspace);
+	QTest::newRow("there and back again") << list2 << "";
+}
+
+void Initial::testGui() {
+	QFETCH(QTestEventList, events);
+	QFETCH(QString, expected);
+
+	QLineEdit lineEdit;
+
+	events.simulate(&lineEdit);
+
+	QCOMPARE(lineEdit.text(), expected);
+
+	QBENCHMARK {
+		int result = (lineEdit.text() == expected);
+	}
 }
 
 QTEST_MAIN(Initial)
--- bulmages/tests/test_initial.h
+++ bulmages/tests/test_initial.h
@@ -1,11 +1,19 @@
 #ifndef __INITIAL__
 #define __INITIAL__
-#include<QtTest>
 #include<QtCore>
+#include<QtGui>
+#include<QtTest>
 
 class Initial: public QObject {
 	Q_OBJECT
 	private slots:
+
+	// Basic string testing
+	void toUpper_data();
 	void toUpper();
+
+	// Basic UI testing
+	void testGui_data();
+	void testGui();
 };
 #endif //__INITIAL__
