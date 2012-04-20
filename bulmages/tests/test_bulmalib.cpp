825da4e5b529c9f3b2c4efe6f7c9777db6afda02
Pablo Alvarez de Sotomayor Posadillo <palvarez@ritho.net>
2012-04-20 17:59:57 +0200

    Added a test for bulmalib.

---
+++ bulmages/tests/test_bulmalib.cpp
@@ -0,0 +1,1 @@
+#include "test_bulmalib.h"
---
+++ bulmages/tests/test_bulmalib.h
@@ -0,0 +1,11 @@
+#ifndef __BULMALIB_H__
+#define __BULMALIB_H__
+#include<QtCore>
+#include<QtGui>
+#include<QtTest>
+
+class TestBulmalib: public QObject {
+	Q_OBJECT
+	private slots:
+};
+#endif //__BULMALIB_H__
