cea0d6685bca1e74f29250083b6635d559de295d
Tomeu Borras <tborras@conetxia.com>
2012-05-17 17:17:49 +0200

    Arreglo problema con las amortizaciones y los asientos inteligentes. No se podian generar automaticamente los asientos de amortizacion. Ahora ya se puede mediante el menu contextual

--- bulmages/bulmacont/plugins/pluginbc_amortizacion/bcamortizacionview.cpp
+++ bulmages/bulmacont/plugins/pluginbc_amortizacion/bcamortizacionview.cpp
@@ -95,6 +95,7 @@ BcAmortizacionView::BcAmortizacionView ( BcCompany *company, QWidget *parent )
     mui_listaCuotas->addSubFormHeader ( "fechaprevista", BlDbField::DbDate, BlDbField::DbNotNull, BlSubFormHeader::DbNone , _ ( "Fecha prevista" ) );
     mui_listaCuotas->addSubFormHeader ( "cantidad", BlDbField::DbNumeric, BlDbField::DbNotNull, BlSubFormHeader::DbNone, _ ( "Cantidad" ) );
     mui_listaCuotas->addSubFormHeader ( "idasiento", BlDbField::DbInt, BlDbField::DbNothing, BlSubFormHeader::DbNoWrite , _ ( "Id asiento" ) );
+    mui_listaCuotas->addSubFormHeader ( "ordenasiento", BlDbField::DbInt, BlDbField::DbNoSave, BlSubFormHeader::DbNoWrite , _ ( "Num. asiento" ) );
     mui_listaCuotas->addSubFormHeader ( "idlinamortizacion", BlDbField::DbInt, BlDbField::DbPrimaryKey, BlSubFormHeader::DbNoWrite , _ ( "Id lineas de amortizacion" ) );
     mui_listaCuotas->addSubFormHeader ( "idamortizacion", BlDbField::DbInt, BlDbField::DbNotNull, BlSubFormHeader::DbNoWrite , _ ( "Id amortizacion" ) );
     mui_listaCuotas->setInsert ( FALSE );
@@ -524,9 +525,7 @@ void BcAmortizacionSubForm::execMenuAction ( QAction *opcion )
     if ( opcion->text() == _ ( "Generar asiento" ) ) {
         /// Se va a generar el asiento.
         QString fecha = dbValue ( "fechaprevista" );
-        //            fprintf(stderr, "Fecha: %s\n", fecha.toAscii().constData());
         QString cant = dbValue ( "cantidad" );
-        //            fprintf(stderr, "Cuota: %s\n", cant.toAscii().constData());
 
         QString cuenta, cuentaamort;
         QString query = "SELECT idcuentaactivo, idcuentaamortizacion FROM amortizacion WHERE idamortizacion=" + dbValue ( "idamortizacion" );
@@ -557,8 +556,6 @@ void BcAmortizacionSubForm::execMenuAction ( QAction *opcion )
         BcAsientoInteligenteView *nueva = (BcAsientoInteligenteView *) g_plugParams;
 	
 	nueva->show();
-	
-	blMsgInfo("Aqui estoy");
 	
         nueva->inicializa ( 0 );
 
@@ -600,7 +597,10 @@ void BcAmortizacionSubForm::execMenuAction ( QAction *opcion )
         QString idlinamortizacion = dbValue ( "idlinamortizacion" );
         SQLQuery = "UPDATE linamortizacion set idasiento = " + QString::number ( numasiento1 ) + " WHERE idlinamortizacion = " + idlinamortizacion;
         mainCompany() ->runQuery ( SQLQuery );
+	
+	setDbValue("idasiento", currentRow(), QString::number ( numasiento1 ));
+	setDbValue("ordenasiento",currentRow(), ordenasiento);
+	
     } // end if
-//    on_mui_confquery_clicked();
 }
 
