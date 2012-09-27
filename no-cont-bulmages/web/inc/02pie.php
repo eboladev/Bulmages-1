8ad282ad5dc2924e6afd3ae99ff1d3cbbf265e8c
Tomeu Borras <tborras@conetxia.com>
2012-09-27 21:59:10 +0200

    Quito directorio de discontinuados que estan tan obsoletos que seria mejor empezarlos desde cero

--- no-cont-bulmages/web/inc/02pie.php
+++
@@ -1,7 +0,0 @@
-<table width="684" border="0" align="center" cellpadding="0" cellspacing="0">
-<tr>
-    <td align="right"> <font size="1" face="Verdana, Arial, Helvetica, sans-serif">Questions, 
-      suggestions or comments about the project?. Write to our <a href="links.php"> 
-      mailing list.</a>. </font></td>
-</tr>
-</table>
