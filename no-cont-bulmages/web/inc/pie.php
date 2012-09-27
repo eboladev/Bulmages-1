a86bd55ad4909ddcbe0a9937d6cc3db28a725e02
Tomeu Borras <tborras@conetxia.com>
2012-09-27 21:59:10 +0200

    Quito directorio de discontinuados que estan tan obsoletos que seria mejor empezarlos desde cero

--- no-cont-bulmages/web/inc/pie.php
+++
@@ -1,7 +0,0 @@
-<table width="684" border="0" align="center" cellpadding="0" cellspacing="0">
-<tr>
-    <td align="right"> <font size="1" face="Verdana, Arial, Helvetica, sans-serif">¿Preguntas, 
-      sugerencias o comentarios sobre el proyecto?. Escriba a nuestra <a href="links.php"> 
-      lista de distribución</a>.<br> <A HREF="http://www.iglues.org">Asociaci&oacute;n Iglues</A>, Septiempbre 2004</font></td>
-</tr>
-</table>
